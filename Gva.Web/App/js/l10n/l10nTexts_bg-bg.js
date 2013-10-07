4dd2bfc79b095dd16eee1e5f788fd87c57c8f321
PankaLM <pan_lm@abv.bg>
2013-10-07 11:27:44 +0300

    Added prefix "users" to the l10n labels in the users views.

--- Gva.Web/App/js/l10n/l10nTexts_bg-bg.js
+++ Gva.Web/App/js/l10n/l10nTexts_bg-bg.js
@@ -13,41 +13,43 @@
         license: 'Лицензиране на авиационен персонал,' +
           ' въздухоплавателни средства и летателна годност'
       },
-      search: {
-        username: 'Потребителско име',
-        name: 'Име',
-        activity: 'Активност',
-        onlyActive: 'Само активни',
-        onlyUnactive: 'Само неактивни',
-        roles: 'Роли',
-        active: 'Активен',
-        noUsersFound: 'Няма намерени потребители',
-        yes: 'Да',
-        no: 'Не',
-        'new': 'Нов потребител',
-        search: 'Търси',
-        edit: 'Редакция'
-      },
-      edit: {
-        username: 'Потребителско име:',
-        name: 'Име:',
-        usernameInvalid: 'потребителското име трябва да е поне 5 символа' +
-          ' и да съдържа само букви, числа, подчертавки (_) и точки (.)',
-        usernameExists: 'потребителското име е заето',
-        comment: 'Коментар:',
-        entrance:'Вход:',
-        withPassAndUsername: ' с потребителско име / парола',
-        withCertificate: ' със сертификат',
-        password: 'Парола:',
-        passMustBeMin8symbols: 'паролата трябва да бъде поне 8 символа',
-        repeatPass: 'Повтори парола:',
-        doNotMatch: 'паролите не съвпадат',
-        certificate: 'Сертификат:',
-        inputCertificate: 'въведете сертификат',
-        roles: 'Роли:',
-        active: 'Активен:',
-        save: 'Запис',
-        cancel: 'Отказ'
+      users: {
+        search: {
+          username: 'Потребителско име',
+          name: 'Име',
+          activity: 'Активност',
+          onlyActive: 'Само активни',
+          onlyUnactive: 'Само неактивни',
+          roles: 'Роли',
+          active: 'Активен',
+          noUsersFound: 'Няма намерени потребители',
+          yes: 'Да',
+          no: 'Не',
+          'new': 'Нов потребител',
+          search: 'Търси',
+          edit: 'Редакция'
+        },
+        edit: {
+          username: 'Потребителско име:',
+          name: 'Име:',
+          usernameInvalid: 'потребителското име трябва да е поне 5 символа' +
+            ' и да съдържа само букви, числа, подчертавки (_) и точки (.)',
+          usernameExists: 'потребителското име е заето',
+          comment: 'Коментар:',
+          entrance:'Вход:',
+          withPassAndUsername: ' с потребителско име / парола',
+          withCertificate: ' със сертификат',
+          password: 'Парола:',
+          passMustBeMin8symbols: 'паролата трябва да бъде поне 8 символа',
+          repeatPass: 'Повтори парола:',
+          doNotMatch: 'паролите не съвпадат',
+          certificate: 'Сертификат:',
+          inputCertificate: 'въведете сертификат',
+          roles: 'Роли:',
+          active: 'Активен:',
+          save: 'Запис',
+          cancel: 'Отказ'
+        }
       }
     });
   }]);
