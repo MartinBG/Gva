f48ff74ecea8c670b4aee3b2d07c1be963acda42
PankaLM <pan_lm@abv.bg>
2013-10-08 10:46:37 +0300

    Implemented sc-select directive.

---
+++ Gva.Web/App/js/scaffolding/directives/scSelectDirective.js
@@ -0,0 +1,21 @@
+﻿//Usage: <sc-select ng-model="<model_name>"/></sc-select>
+(function (angular) {
+  'use strict';
+
+  function scSelectDirective() {
+    return {
+      restrict: 'E',
+      replace: true,
+      require: ['?ngModel', 'uiSelect2'],
+      templateUrl: 'scaffolding/templates/scSelectTemplate.html',
+      link: function (scope, element, attrs, ngModel) {
+        if (!ngModel) {
+          return;
+        }
+
+      }
+    };
+  }
+
+  angular.module('scaffolding').directive('scSelect', scSelectDirective);
+}(angular));
