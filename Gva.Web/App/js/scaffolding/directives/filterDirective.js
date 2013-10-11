efc02d8ed07e55eef66a241bddddb748c7f170f0
akkumanova <akumanova@yahoo.com>
2013-10-11 14:06:20 +0300

    Fixed sc-search style

--- Gva.Web/App/js/scaffolding/directives/filterDirective.js
+++ Gva.Web/App/js/scaffolding/directives/filterDirective.js
@@ -4,9 +4,57 @@
   'use strict';
 
   function FilterDirective() {
+    function FilterCompile (tElement, tAttrs) {
+      var type = tAttrs.type,
+          options = tAttrs.options;
+
+      if (!tAttrs.classes) {
+        tAttrs.classes = 'col-sm-3';
+      }
+
+      if (type === 'date' ||
+          type === 'int' ||
+          type === 'float' ||
+          type === 'text' ||
+          type === 'select') {
+        var dirHtml, optionsHtml;
+
+        optionsHtml = options ? ' sc-' + type + '="options"' : '';
+        dirHtml = '<sc-' + type + ' ng-model="model"' + optionsHtml + '></sc-' + type + '>';
+
+        tElement.append(dirHtml);
+      }
+
+      return FilterLink;
+    }
+
+    function FilterLink($scope, element, attrs, scSearch) {
+      if (!scSearch) {
+        return;
+      }
+
+      var modelName = attrs.modelName;
+      $scope.model = scSearch.selectedFilters[modelName];
+      $scope.show = function () {
+        return modelName in scSearch.selectedFilters;
+      };
+
+      if ($scope.model !== undefined) {
+        $scope.$watch('model', function (value) {
+          scSearch.selectedFilters[modelName] = value;
+        });
+      }
+
+      $scope.removeFilter = function () {
+        return scSearch.removeFilter($scope.filterName, modelName);
+      };
+
+      scSearch.registerFilter(modelName, $scope);
+    }
+
     return {
       restrict: 'E',
-      require: '^scSearch',
+      require: '?^scSearch',
       replace: true,
       scope: {
         type: '@',
@@ -17,59 +65,7 @@
         filterName: '@'
       },
       templateUrl: 'scaffolding/templates/filterTemplate.html',
-      link: function (scope, element, attrs, scSearch) {
-        if (!scSearch) {
-          return;
-        }
-
-        var modelName = attrs.modelName;
-        scope.model = scSearch.selectedFilters[modelName];
-        scope.show = function () {
-          return modelName in scSearch.selectedFilters;
-        };
-
-        if (scope.model !== undefined) {
-          scope.$watch('model', function (value) {
-            scSearch.selectedFilters[modelName] = value;
-          });
-        }
-
-        scope.removeFilter = function () {
-          if (scope.filterName) {
-            scSearch.nonSelectedFilters.push({
-              name: scope.filterName,
-              model: modelName
-            });
-          }
-
-          delete scSearch.selectedFilters[modelName];
-        };
-
-        scSearch.addFilter(modelName, scope);
-      },
-      compile: function (tElement, tAttrs) {
-        var type = tAttrs.type,
-            options = tAttrs.options;
-
-        if (!tAttrs.classes) {
-          tAttrs.classes = 'col-sm-3';
-        }
-
-        if (type === 'date' ||
-            type === 'int' ||
-            type === 'float' ||
-            type === 'text' ||
-            type === 'select') {
-          var dirHtml, optionsHtml;
-
-          optionsHtml = options ? ' sc-' + type + '="options"' : '';
-          dirHtml = '<sc-' + type + ' ng-model="model"' + optionsHtml + '></sc-' + type + '>';
-
-          tElement.append(dirHtml);
-        }
-
-        return this.link;
-      }
+      compile: FilterCompile
     };
   }
 
