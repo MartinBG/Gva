3e742485ea577333e4592f695b1a04ebb33d899b
dimitar.kaludov <mitakyt@gmail.com>
2013-11-07 16:20:53 +0200

    Initial commit

---
+++ Gva.Web/App/test/e2e/directives/sc-files/controllers/scFilesCtrl.js
@@ -0,0 +1,33 @@
+﻿(function (angular) {
+  'use strict';
+
+  function ScFilesCtrl($scope) {
+    $scope.files = [];
+
+    var multipleFiles = [
+      { key: 'f111111111', relativePath: 'app/', name: 'file1' },
+      { key: 'f222222222', relativePath: 'app/', name: 'file2' },
+      { key: 'f333333333', relativePath: 'app/', name: 'file3' },
+      { key: 'f444444444', relativePath: '', name: 'file4' }
+    ];
+    var singleFile = [
+      { key: 'f111111111', relativePath: 'app/', name: 'file1' }
+    ];
+
+    $scope.changeToMultipleFiles = function () {
+      $scope.files = multipleFiles;
+    };
+
+    $scope.changeToSingleFile = function () {
+      $scope.files = singleFile;
+    };
+
+    $scope.changeToNoFiles = function () {
+      $scope.files = [];
+    };
+  }
+
+  ScFilesCtrl.$inject = ['$scope'];
+
+  angular.module('directive-tests').controller('directive-tests.ScFilesCtrl', ScFilesCtrl);
+}(angular));
---
+++ Gva.Web/App/test/e2e/directives/sc-files/specs/filesSpec.js
@@ -0,0 +1,68 @@
+﻿/*global protractor, describe, beforeEach, it, expect*/
+(function (protractor) {
+  'use strict';
+
+  describe('Sc-files directive', function () {
+    var ptor = protractor.getInstance(),
+      filesDirectiveSpan,
+      singleFileDirectiveSpan,
+      multipleFilesBtnElem,
+      singleFileBtnElem,
+      noFilesBtnElem,
+      openModalBtnElem;
+
+
+    beforeEach(function () {
+      ptor.get('#/test/files');
+      ptor.waitForAngular();
+
+      /*jshint quotmark:false */
+      filesDirectiveSpan = ptor.findElement(
+        protractor.By.xpath("//div[@class='form-control']/span"));
+      singleFileDirectiveSpan = ptor.findElement(
+        protractor.By.xpath("//div[@class='form-control']/a/span"));
+      multipleFilesBtnElem = ptor.findElement(protractor.By.name('multipleFilesBtn'));
+      singleFileBtnElem = ptor.findElement(protractor.By.name('singleFileBtn'));
+      noFilesBtnElem = ptor.findElement(protractor.By.name('noFilesBtn'));
+      openModalBtnElem = ptor.findElement(protractor.By.className('glyphicon-file'));
+    });
+
+    it('should set the text of the element to whatever file array is passed.', function () {
+
+      var multipleFilesBtnElem = ptor.findElement(protractor.By.name('multipleFilesBtn'));
+      var singleFileBtnElem = ptor.findElement(protractor.By.name('singleFileBtn'));
+      var noFilesBtnElem = ptor.findElement(protractor.By.name('noFilesBtn'));
+
+      filesDirectiveSpan.getText().then(function (value) {
+        expect(value).toEqual('Няма прикачени файлове.');
+      });
+
+      multipleFilesBtnElem.click();
+      filesDirectiveSpan.getText().then(function (value) {
+        expect(value).toEqual('4 прикачени файла.');
+      });
+
+      singleFileBtnElem.click();
+      singleFileDirectiveSpan.getText().then(function (value) {
+        expect(value).toEqual('file1');
+      });
+
+      noFilesBtnElem.click();
+      filesDirectiveSpan.getText().then(function (value) {
+        expect(value).toEqual('Няма прикачени файлове.');
+      });
+    });
+
+    it('should display files.', function () {
+      /*jshint quotmark:false */
+
+      openModalBtnElem.click().then(function () {
+        ptor.findElement(
+            protractor.By.xpath("//div[@class='modal-body ng-scope']/span[last()]")
+          ).getText().then(function (value) {
+            expect(value).toEqual('Няма прикачени файлове');
+          });
+      });
+    });
+  });
+}(protractor));
