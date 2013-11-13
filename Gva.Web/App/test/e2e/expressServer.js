46d4f82ddb16f4b1d11357240cdf2b411ab5e436
dimitar.kaludov <mitakyt@gmail.com>
2013-11-13 11:50:32 +0200

    Files download with express server

--- Gva.Web/App/test/e2e/expressServer.js
+++ Gva.Web/App/test/e2e/expressServer.js
@@ -1,42 +1,51 @@
-﻿/*global require, module, console, it, expect*/
+﻿/*global require, module, setTimeout*/
+(function () {
+  'use strict';
+  var express = require('express');
+  var fs = require('fs');
+  var app = express();
+  var files = {};
 
-var express = require('express');
-var fs = require('fs');
-var app = express();
-var files = {};
-
-function generateGuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-  });
-}
-
-function processFile(fileKey, reqFile, res) {
-  fs.readFile(reqFile.path, function (err, data) {
-    files[fileKey] = data;
-    fs.unlink(reqFile.path, function() {
-      if (err) throw err;
-      res.send(JSON.stringify({fileKey: fileKey}));
+  function generateGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
     });
-  });
-}
-
-app.use(express.multipart());
-
-app.post('/file', function(req, res) {
-  var fileKey = generateGuid();
-  var currentFile = req.files.files[0];
-
-  if (currentFile.name === 'filesModalCtrl.js') {
-    console.log('if');
-    setTimeout(function(){
-      processFile(fileKey, currentFile, res);
-    }, 9000);
-  } else {
-    console.log('else');
-    processFile(fileKey, currentFile, res);
   }
-});
-module.exports = app;
+
+  function processFile(fileKey, reqFile, res) {
+    fs.readFile(reqFile.path, function (err, data) {
+      files[fileKey] = data;
+      fs.unlink(reqFile.path, function() {
+        if (err) { throw err; }
+        res.send(JSON.stringify({fileKey: fileKey}));
+      });
+    });
+  }
+
+  app.use(express.multipart());
+
+  app.post('/file', function(req, res) {
+    var fileKey = generateGuid();
+    var currentFile = req.files.files[0];
+
+    if (currentFile.name === 'filesDirective.js') {
+      setTimeout(function(){
+        processFile(fileKey, currentFile, res);
+      }, 3000);
+    } else {
+      processFile(fileKey, currentFile, res);
+    }
+  });
+  
+  app.get('/file', function(req, res) {
+    var fileName = req.query.fileName,
+      fileKey = req.query.fileKey;
+    var file = files[fileKey];
+    res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+    res.setHeader('Content-type', 'application/octet-stream');
+    res.send(file);  
+  });
+  module.exports = app;
+}());
 
