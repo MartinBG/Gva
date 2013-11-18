2d3a6e019d2bd69bf320d4896ed8fe1fdf2cd0aa
akkumanova <akumanova@yahoo.com>
2013-11-18 17:47:06 +0200

    Reorganized tests

--- Gva.Model.Tests/Tests/LotTests.cs
+++ Gva.Model.Tests/Tests/LotTests.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Common.Data;
 using Common.Infrastructure;
 using Gva.Model.Tests.Common;
@@ -13,10 +14,11 @@ using Regs.Api.Managers.LotManager;
 using Regs.Api.Models;
 using Xunit;
 using Xunit.Extensions;
+using System.Data.Entity;
 
-namespace Gva.Model.Tests
+namespace Gva.Model.Tests.Tests
 {
-    public class Test
+    public class LotTests
     {
         private IKernel kernel;
         private IUnitOfWork unitOfWork;
@@ -24,7 +26,7 @@ namespace Gva.Model.Tests
         private ILobManager lobManager;
         private UserContext userContext;
 
-        public Test()
+        public LotTests()
         {
             this.kernel = new StandardKernel();
 
@@ -64,37 +66,6 @@ namespace Gva.Model.Tests
         }
 
         [Fact, AutoRollback]
-        public void AddPart()
-        {
-            using (var transaction = this.unitOfWork.BeginTransaction())
-            {
-                Set set = this.lotManager.GetSet("Person");
-
-                Lot lot = set.AddLot(this.userContext);
-                lot.AddPart(
-                    "/addresses/*",
-                    JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
-                    this.lobManager,
-                    this.userContext);
-                this.unitOfWork.Save();
-
-                lot = this.lotManager.GetLot(lot.LotId);
-                PartVersion savedPartVersion = lot.GetPart("/addresses/0");
-
-                Part part = this.unitOfWork.DbContext.Set<Part>().FirstOrDefault(p => p.Path == "/addresses/0");
-                TextBlob textBlob = this.unitOfWork.DbContext.Set<TextBlob>().FirstOrDefault(tb => tb.Hash == "876FB827A153BB2CB30FC25B082B7CB9D31290D8");
-                Commit commit = lot.Commits.First();
-
-                Assert.Equal<int>(part.PartId, savedPartVersion.PartId);
-                Assert.Equal<int>(textBlob.TextBlobId, savedPartVersion.TextBlobId);
-                Assert.Equal<int>(commit.CommitId, savedPartVersion.OriginalCommitId);
-                Assert.Equal<int>(this.userContext.UserId, savedPartVersion.CreatorId);
-                Assert.Equal<int>((int)PartOperation.Add, (int)savedPartVersion.PartOperation);
-                Assert.Contains<PartVersion>(savedPartVersion, commit.PartVersions);
-            }
-        }
-
-        [Fact, AutoRollback]
         public void CommitAllParts()
         {
             using (var transaction = this.unitOfWork.BeginTransaction())
@@ -107,6 +78,7 @@ namespace Gva.Model.Tests
                     JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
                     this.lobManager,
                     this.userContext);
+                this.unitOfWork.Save();
                 lot.AddPart(
                     "/addresses/*",
                     JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
@@ -153,6 +125,7 @@ namespace Gva.Model.Tests
                     JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
                     this.lobManager,
                     this.userContext);
+                this.unitOfWork.Save();
                 lot.AddPart(
                     "/addresses/*",
                     JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
@@ -187,97 +160,6 @@ namespace Gva.Model.Tests
                 var commitedPv = oldCommit.PartVersions.ToArray();
                 Assert.Equal<int>(1, commitedPv.Length);
                 Assert.Equal<string>("/addresses/1", commitedPv[0].Part.Path);
-            }
-        }
-
-        [Fact, AutoRollback]
-        public void UpdatePart()
-        {
-            using (var transaction = this.unitOfWork.BeginTransaction())
-            {
-                Set set = this.lotManager.GetSet("Person");
-
-                Lot lot = set.AddLot(this.userContext);
-                lot.AddPart(
-                    "/addresses/*",
-                    JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
-                    this.lobManager,
-                    this.userContext);
-                lot.AddPart(
-                    "/addresses/*",
-                    JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
-                    this.lobManager,
-                    this.userContext);
-                this.unitOfWork.Save();
-
-                lot = this.lotManager.GetLot(lot.LotId);
-                lot.Commit(this.userContext);
-                this.unitOfWork.Save();
-
-                lot.UpdatePart("/addresses/1", new JObject(), this.lobManager, this.userContext);
-                this.unitOfWork.Save();
-
-                Commit index = this.unitOfWork.DbContext.Set<Commit>()
-                    .Include(c => c.PartVersions)
-                    .FirstOrDefault(c => c.IsIndex);
-
-                PartVersion updatedPV = index.PartVersions.FirstOrDefault(pv => pv.PartOperation == PartOperation.Update);
-                Assert.NotNull(updatedPV);
-                Assert.Equal<int>(index.CommitId, updatedPV.OriginalCommitId);
-
-                PartVersion oldPV = index.PartVersions.FirstOrDefault(pv => pv.PartId == updatedPV.PartId && pv.PartOperation != PartOperation.Update);
-                Assert.Null(oldPV);
-            }
-        }
-
-        [Fact, AutoRollback]
-        public void DeletePart()
-        {
-            using (var transaction = this.unitOfWork.BeginTransaction())
-            {
-                Set set = this.lotManager.GetSet("Person");
-
-                Lot lot = set.AddLot(this.userContext);
-                lot.AddPart(
-                    "/addresses/*",
-                    JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
-                    this.lobManager,
-                    this.userContext);
-                lot.AddPart(
-                    "/addresses/*",
-                    JObject.Parse(@"{ key1: 'value1', key2: [ 'value1', 'value2']}"),
-                    this.lobManager,
-                    this.userContext);
-                this.unitOfWork.Save();
-
-                lot = this.lotManager.GetLot(lot.LotId);
-                lot.Commit(this.userContext);
-                this.unitOfWork.Save();
-
-                lot.DeletePart("/addresses/1", this.userContext);
-                this.unitOfWork.Save();
-
-                Commit index = this.unitOfWork.DbContext.Set<Commit>()
-                    .Include(c => c.PartVersions)
-                    .FirstOrDefault(c => c.IsIndex);
-
-                PartVersion deletedPV = index.PartVersions.FirstOrDefault(pv => pv.PartOperation == PartOperation.Delete);
-                Assert.NotNull(deletedPV);
-                Assert.Equal<int>(index.CommitId, deletedPV.OriginalCommitId);
-
-                PartVersion oldPV = index.PartVersions.FirstOrDefault(pv => pv.PartId == deletedPV.PartId && pv.PartOperation != PartOperation.Delete);
-                Assert.Null(oldPV);
-
-                lot.Commit(this.userContext);
-                this.unitOfWork.Save();
-
-                index = this.unitOfWork.DbContext.Set<Commit>()
-                    .Include(c => c.PartVersions)
-                    .FirstOrDefault(c => c.IsIndex);
-                var indexPVs = index.PartVersions.ToArray();
-
-                Assert.Equal<int>(1, indexPVs.Length);
-                Assert.Equal<string>("/addresses/0", indexPVs[0].Part.Path);
             }
         }
     }
