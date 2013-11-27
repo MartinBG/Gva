63ac178049518c05b1fd2407586d56c345b784ca
Georgi Yordanov <georgi.yordanov@gmail.com>
2013-11-27 19:11:05 +0200

    Fixed promiseStateDirective. Resolving/rejecting an old promise no longer changes the icon of the directive.

--- Gva.Web/App/js/scaffolding/directives/promiseState/promiseStateDirective.js
+++ Gva.Web/App/js/scaffolding/directives/promiseState/promiseStateDirective.js
@@ -17,19 +17,38 @@
         scope.state = null;
         scope.title = '';
 
+        var cancellationToken;
         scope.$watch('promise', function () {
+          if (cancellationToken) {
+            cancellationToken.isCancelled = true;
+          }
+
           if (scope.promise) {
             scope.state = 'loading';
             scope.title = '';
 
-            scope.promise
-              .then(function (result) {
-                scope.state = 'resolved';
-                scope.title = result;
-              }, function (reason) {
-                scope.state = 'rejected';
-                scope.title = reason;
-              });
+            cancellationToken = { isCancelled: false };
+
+            (function () {
+              var c = cancellationToken;
+
+              scope.promise
+                .then(function (result) {
+                  if (c.isCancelled) {
+                    return;
+                  }
+
+                  scope.state = 'resolved';
+                  scope.title = result;
+                }, function (reason) {
+                  if (c.isCancelled) {
+                    return;
+                  }
+
+                  scope.state = 'rejected';
+                  scope.title = reason;
+                });
+            })();
           } else {
             scope.state = null;
             scope.title = '';
