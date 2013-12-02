4e512b351e886e89d1ba0f9368e01887305bd0b1
Georgi Yordanov <georgi.yordanov@gmail.com>
2013-12-02 13:46:27 +0200

    Added $delay service.

---
+++ Gva.Web/App/test/e2e/delay.js
@@ -0,0 +1,12 @@
+﻿/*global angular*/
+(function (angular) {
+  'use strict';
+
+  angular.module('app').factory('$delay', ['$timeout', function ($timeout) {
+    return function (delay, value) {
+      return $timeout(function() {
+        return value;
+      }, delay);
+    };
+  }]);
+}(angular));
