386a4475a42e03931220a00c28c7b1c80521b41b
Georgi Yordanov <georgi.yordanov@gmail.com>
2013-12-02 11:07:44 +0200

    Added scaffoldingProvider.

---
+++ Gva.Web/App/js/scaffolding/providers/scaffoldingProvider.js
@@ -0,0 +1,30 @@
+﻿/*global angular*/
+(function (angular) {
+  'use strict';
+
+  function ScaffoldingProvider($compileProvider) {
+    this.form = function (options) {
+      $compileProvider.directive(options.name, function () {
+        return {
+          restrict: 'E',
+          replace: true,
+          scope: {
+            model: '=ngModel'
+          },
+          templateUrl: options.templateUrl,
+          controller: options.controller,
+          link: function (scope, element, attrs) {
+            scope.$parent[attrs.name] = scope[attrs.name];
+          }
+        };
+      });
+    };
+  }
+
+  ScaffoldingProvider.$inject = ['$compileProvider'];
+
+  ScaffoldingProvider.prototype.$get = function () {
+  };
+
+  angular.module('scaffolding').provider('scaffolding', ScaffoldingProvider);
+}(angular));
