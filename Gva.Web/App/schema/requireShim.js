1477d304cc20563fb259077bbbe880ef7611556f
PankaLM <pan_lm@abv.bg>
2013-12-03 13:11:09 +0200

    Changed project to use source maps and source files with hash at debug.

---
+++ Gva.Web/App/schema/requireShim.js
@@ -0,0 +1,8 @@
+﻿window.require = function (module) {
+  var match = /(\.\/)?(.+)/.exec(module);
+  if (match) {
+    return window[match[2]].exports;
+  }
+  return undefined;
+}
+window.getData = window.require;
