d0432d31e9545862f14d0d16fa44aee0a7fdc2bf
Angel Yordanov <yordanov@gmail.com>
2013-12-13 15:11:11 +0200

    Fix Common.Data.Events thread static initialization

--- Common/Data/Events.cs
+++ Common/Data/Events.cs
@@ -7,18 +7,38 @@ using System.Threading.Tasks;
 namespace Common.Data
 {
     public static class Events
-    { 
+    {
         [ThreadStatic]
-        private static IList<IEventHandler> handlers = new List<IEventHandler>();
+        private static IList<IEventHandler> handlers;
+
+        private static IList<IEventHandler> Handlers
+        {
+            get
+            {
+                //Initializing the static field through a property.
+
+                //From MSDN:
+                //Do not specify initial values for fields marked with ThreadStaticAttribute,
+                //because such initialization occurs only once, when the class constructor executes
+                //and therefore affects only one thread.
+
+                if (handlers == null)
+                {
+                    handlers = new List<IEventHandler>();
+                }
+
+                return handlers;
+            }
+        }
 
         public static void Register(IEventHandler handler)
         {
-            handlers.Add(handler);
+            Handlers.Add(handler);
         }
 
         public static void Raise(IEvent e)
         {
-            foreach (var handler in handlers)
+            foreach (var handler in Handlers)
             {
                 handler.Handle(e);
             }
