e9941642d9a4003ff6088020d69a56e6ec2d9fc3
Angel Yordanov <yordanov@gmail.com>
2013-12-16 18:02:54 +0200

    Fix protractor locators to work element.findElements to find descendant elements

--- Gva.Web/App/test/e2e/gvaBy.js
+++ Gva.Web/App/test/e2e/gvaBy.js
@@ -43,27 +43,29 @@
     }
 
     return {
-      findOverride: function(driver) {
-        return driver.findElement(protractor.By.css(datatableContainerCss));
+      findOverride: function(driver, element) {
+        return (element || driver).findElement(protractor.By.css(datatableContainerCss));
       },
-      findArrayOverride: function(driver) {
-        return driver.findElements(protractor.By.css(datatableContainerCss));
+      findArrayOverride: function(driver, element) {
+        return (element || driver).findElements(protractor.By.css(datatableContainerCss));
       },
       row: function (index) {
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.css(createCellSelector(index)));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.css(createCellSelector(index)));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.css(createCellSelector(index)));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.css(createCellSelector(index)));
           },
           column: function (data) {
             return {
-              findOverride: function(driver) {
-                return driver.findElement(protractor.By.css(createCellSelector(index, data)));
+              findOverride: function(driver, element) {
+                return (element || driver)
+                  .findElement(protractor.By.css(createCellSelector(index, data)));
               },
-              findArrayOverride: function(driver) {
-                return driver.findElements(protractor.By.css(createCellSelector(index, data)));
+              findArrayOverride: function(driver, element) {
+                return (element || driver)
+                  .findElements(protractor.By.css(createCellSelector(index, data)));
               }
             };
           }
@@ -71,29 +73,33 @@
       },
       column: function(data) {
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.css(createCellSelector(undefined, data)));
+          findOverride: function(driver, element) {
+            return (element || driver)
+              .findElement(protractor.By.css(createCellSelector(undefined, data)));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.css(createCellSelector(undefined, data)));
+          findArrayOverride: function(driver, element) {
+            return (element || driver)
+              .findElements(protractor.By.css(createCellSelector(undefined, data)));
           }
         };
       },
       header: function() {
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.css(createCellSelector(0)));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.css(createCellSelector(0)));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.css(createCellSelector(0)));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.css(createCellSelector(0)));
           },
           column: function (data) {
             return {
-              findOverride: function(driver) {
-                return driver.findElement(protractor.By.css(createCellSelector(0, data)));
+              findOverride: function(driver, element) {
+                return (element || driver)
+                  .findElement(protractor.By.css(createCellSelector(0, data)));
               },
-              findArrayOverride: function(driver) {
-                return driver.findElements(protractor.By.css(createCellSelector(0, data)));
+              findArrayOverride: function(driver, element) {
+                return (element || driver)
+                  .findElements(protractor.By.css(createCellSelector(0, data)));
               }
             };
           }
@@ -104,11 +110,11 @@
           datatableContainerCss +
           ' div[class=dataTables_filter] input';
         return {
-          findOverride: function (driver) {
-            return driver.findElement(protractor.By.css(filterInputCss));
+          findOverride: function (driver, element) {
+            return (element || driver).findElement(protractor.By.css(filterInputCss));
           },
-          findArrayOverride: function (driver) {
-            return driver.findElements(protractor.By.css(filterInputCss));
+          findArrayOverride: function (driver, element) {
+            return (element || driver).findElements(protractor.By.css(filterInputCss));
           }
         };
       },
@@ -118,11 +124,11 @@
           ' div[class=dataTables_info]';
 
         return {
-          findOverride: function (driver) {
-            return driver.findElement(protractor.By.css(infoTextCss));
+          findOverride: function (driver, element) {
+            return (element || driver).findElement(protractor.By.css(infoTextCss));
           },
-          findArrayOverride: function (driver) {
-            return driver.findElements(protractor.By.css(infoTextCss));
+          findArrayOverride: function (driver, element) {
+            return (element || driver).findElements(protractor.By.css(infoTextCss));
           }
         };
       },
@@ -132,11 +138,11 @@
           ' button[data-toggle=dropdown]';
 
         return {
-          findOverride: function (driver) {
-            return driver.findElement(protractor.By.css(hideColumnsButtonCss));
+          findOverride: function (driver, element) {
+            return (element || driver).findElement(protractor.By.css(hideColumnsButtonCss));
           },
-          findArrayOverride: function (driver) {
-            return driver.findElement(protractor.By.css(hideColumnsButtonCss));
+          findArrayOverride: function (driver, element) {
+            return (element || driver).findElement(protractor.By.css(hideColumnsButtonCss));
           }
         };
       },
@@ -148,11 +154,11 @@
           'input[type=checkbox]';
 
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.css(hideColumnCheckboxCss));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.css(hideColumnCheckboxCss));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.css(hideColumnCheckboxCss));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.css(hideColumnCheckboxCss));
           }
         };
       },
@@ -163,22 +169,22 @@
           'select';
 
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.css(lengthFilterCss));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.css(lengthFilterCss));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.css(lengthFilterCss));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.css(lengthFilterCss));
           },
           option: function (number) {
             var lengthFilterOptionCss =
               lengthFilterCss +
               ' option:nth-child(' + (number+1) + ')';
             return {
-              findOverride: function(driver) {
-                return driver.findElement(protractor.By.css(lengthFilterOptionCss));
+              findOverride: function(driver, element) {
+                return (element || driver).findElement(protractor.By.css(lengthFilterOptionCss));
               },
-              findArrayOverride: function(driver) {
-                return driver.findElements(protractor.By.css(lengthFilterOptionCss));
+              findArrayOverride: function(driver, element) {
+                return (element || driver).findElements(protractor.By.css(lengthFilterOptionCss));
               }
             };
           }
@@ -190,11 +196,11 @@
           ' ul[class=pagination] li:nth-child(' + (pageNumber + 1) + ') a';
 
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.css(pageButtonCss));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.css(pageButtonCss));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.css(pageButtonCss));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.css(pageButtonCss));
           }
         };
       }
@@ -204,7 +210,7 @@
   GvaBy.prototype.nomenclature = function(model) {
     /*jshint -W101*/
     /* jshint -W109*/
-    var containerXPath =  "//div[contains(@class,'select2-container') and following-sibling::*[1][self::input and @ng-model='" + model + "']]",
+    var containerXPath =  ".//div[contains(@class,'select2-container') and following-sibling::*[1][self::input and @ng-model='" + model + "']]",
         containerAnchorXPath = containerXPath + "/a[contains(@class, 'select2-choice')]",
         nameSpanXPath = containerAnchorXPath + "/span[contains(@class, 'select2-chosen')]",
         deselectBtnXPath = containerAnchorXPath + "/abbr",
@@ -213,39 +219,39 @@
     /* jshint +W109*/
 
     return {
-      findOverride: function(driver) {
-        return driver.findElement(protractor.By.xpath(containerXPath));
+      findOverride: function(driver, element) {
+        return (element || driver).findElement(protractor.By.xpath(containerXPath));
       },
-      findArrayOverride: function(driver) {
-        return driver.findElements(protractor.By.xpath(containerXPath));
+      findArrayOverride: function(driver, element) {
+        return (element || driver).findElements(protractor.By.xpath(containerXPath));
       },
       text: function () {
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.xpath(nameSpanXPath));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.xpath(nameSpanXPath));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.xpath(nameSpanXPath));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.xpath(nameSpanXPath));
           }
         };
       },
       deselectButton: function () {
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.xpath(deselectBtnXPath));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.xpath(deselectBtnXPath));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.xpath(deselectBtnXPath));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.xpath(deselectBtnXPath));
           }
         };
       },
       dropdownInput: function () {
         return {
-          findOverride: function(driver) {
-            return driver.findElement(protractor.By.xpath(dropdownInputXPath));
+          findOverride: function(driver, element) {
+            return (element || driver).findElement(protractor.By.xpath(dropdownInputXPath));
           },
-          findArrayOverride: function(driver) {
-            return driver.findElements(protractor.By.xpath(dropdownInputXPath));
+          findArrayOverride: function(driver, element) {
+            return (element || driver).findElements(protractor.By.xpath(dropdownInputXPath));
           }
         };
       }
