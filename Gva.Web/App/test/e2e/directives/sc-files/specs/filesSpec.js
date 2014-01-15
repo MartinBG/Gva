94e768b06584c387bb44f5b6185b77b2c939b2a4
dimitar.kaludov <mitakyt@gmail.com>
2014-01-15 16:17:04 +0200

    Rework filesSpec to use page object

--- Gva.Web/App/test/e2e/directives/sc-files/specs/filesSpec.js
+++ Gva.Web/App/test/e2e/directives/sc-files/specs/filesSpec.js
@@ -2,22 +2,20 @@
 (function (protractor, describe, beforeEach, it, expect, require) {
   'use strict';
 
-  var _ = require('lodash');
+  var _ = require('lodash'),
+    ScFiles = require('../../../pageObjects/directives/scFiles.js');
 
   describe('Sc-files directive', function () {
     var ptor = protractor.getInstance(),
-      filesDirectiveSpan,
-      singleFileDirectiveSpan,
       multipleFilesBtnElem,
       singleFileBtnElem,
       noFilesBtnElem,
-      openModalBtnElem;
+      openModalBtnElem,
+      filesDir;
 
     beforeEach(function () {
       ptor.get('#/test/files');
-
-      filesDirectiveSpan = ptor.findElement(protractor.By.className('test-files-span'));
-      singleFileDirectiveSpan = ptor.findElement(protractor.By.className('test-single-file-span'));
+      filesDir = new ScFiles(ptor.findElement(protractor.By.name('filesDir')), ptor);
       multipleFilesBtnElem = ptor.findElement(protractor.By.name('multipleFilesBtn'));
       singleFileBtnElem = ptor.findElement(protractor.By.name('singleFileBtn'));
       noFilesBtnElem = ptor.findElement(protractor.By.name('noFilesBtn'));
@@ -25,88 +23,78 @@
     });
 
     it('should set the text of the element to whatever file array is passed.', function () {
-      filesDirectiveSpan.getText().then(function (value) {
+      filesDir.get().then(function (value) {
         expect(value).toEqual('Няма прикачени файлове.');
       });
 
       multipleFilesBtnElem.click();
-      filesDirectiveSpan.getText().then(function (value) {
+      filesDir.get().then(function (value) {
         expect(value).toEqual('4 прикачени файла.');
       });
 
       singleFileBtnElem.click();
-      singleFileDirectiveSpan.getText().then(function (value) {
+      filesDir.get().then(function (value) {
         expect(value).toEqual('file1');
       });
 
       noFilesBtnElem.click();
-      filesDirectiveSpan.getText().then(function (value) {
+      filesDir.get().then(function (value) {
         expect(value).toEqual('Няма прикачени файлове.');
       });
     });
 
     it('should open modal.', function () {
-      openModalBtnElem.click().then(function () {
-        ptor.findElement(
-            protractor.By.className('test-no-files-span')
-          ).getText().then(function (value) {
-            expect(value).toEqual('Няма прикачени файлове');
-          });
+      filesDir.openModal();
+      filesDir.get().then(function (value) {
+        expect(value).toEqual('Няма прикачени файлове.');
       });
     });
 
     it('should dismiss modal', function () {
       multipleFilesBtnElem.click();
-
-      openModalBtnElem.click().then(function () {
-        return ptor.findElement(protractor.By.className('test-button-close'))
-          .click();
-      })
-      .then(function () {
-        ptor.findElements(protractor.By.className('modal-body'))
-          .then(function (data) {
-            expect(_.keys(data).length).toEqual(0);
-            filesDirectiveSpan.getText().then(function (value) {
-              expect(value).toEqual('4 прикачени файла.');
-            });
+      filesDir.openModal();
+      filesDir.closeModal();
+      ptor.findElements(protractor.By.className('modal-body'))
+        .then(function (data) {
+          expect(_.keys(data).length).toEqual(0);
+          filesDir.get().then(function (value) {
+            expect(value).toEqual('4 прикачени файла.');
           });
-      });
+        });
     });
+
 
     it('should display file tree.', function () {
       multipleFilesBtnElem.click();
-      openModalBtnElem.click().then(function () {
-        ptor.findElements(
-          protractor.By.className('test-files-li')).then(function (data) {
-            expect(_.keys(data).length).toEqual(3);
-          });
-        ptor.findElements(
-          protractor.By.className('test-files-root-li')).then(function (data) {
-            expect(_.keys(data).length).toEqual(2);
-          });
-      });
+      filesDir.openModal();
+      ptor.findElements(
+        protractor.By.className('test-files-li')).then(function (data) {
+          expect(_.keys(data).length).toEqual(3);
+        });
+      ptor.findElements(
+        protractor.By.className('test-files-root-li')).then(function (data) {
+          expect(_.keys(data).length).toEqual(2);
+        });
     });
 
     it('should render download link inside modal.', function () {
       singleFileBtnElem.click();
-      openModalBtnElem.click().then(function () {
-        ptor.findElement(
-          protractor.By.className('test-download-link')).getText().then(function (value) {
-            expect(value).toEqual('file1');
-          });
-      });
+      filesDir.openModal();
+      ptor.findElement(
+        protractor.By.className('test-download-link')).getText().then(function (value) {
+          expect(value).toEqual('file1');
+        });
     });
 
     it('should delete file', function () {
       singleFileBtnElem.click();
-      singleFileDirectiveSpan.getText().then(function (value) {
+      filesDir.get().then(function (value) {
         expect(value).toEqual('file1');
       });
-      openModalBtnElem
-        .click()
-        .then(function () {
-          return ptor.findElement(protractor.By.className('tree-image-close')).click();
-        })
+
+      filesDir.openModal();
+
+      ptor.findElement(protractor.By.className('tree-image-close')).click()
         .then(function () {
           return ptor.findElement(
             protractor.By.className('test-no-files-span')).getText();
@@ -117,62 +105,34 @@
     });
 
     it('should upload file.', function () {
-      openModalBtnElem
-        .click()
-        .then(function () {
-          return ptor.findElement(protractor.By.className('test-file-upload-button'))
-            .sendKeys('//filesCtrl.js');
-        })
-        .then(function () {
-          return ptor.findElement(protractor.By.className('test-file-name')).getText();
-        })
-        .then(function (value) {
-          expect(value).toEqual('filesCtrl.js');
-        })
-        .then(function () {
-          return ptor.findElement(protractor.By.className('btn-primary')).click();
-        })
-        .then(function () {
-          return singleFileDirectiveSpan.getText();
-        })
-        .then(function (value) {
-          expect(value).toEqual('filesCtrl.js');
-        });
+      filesDir.openModal();
+      filesDir.addFile('//filesCtrl.js');
+      filesDir.uploadFiles();
+      filesDir.get().then(function (value) {
+        expect(value).toEqual('filesCtrl.js');
+      });
     });
 
     it('should stop uploading.', function () {
-      openModalBtnElem.click()
-        .then(function () {
-          return ptor.findElement(protractor.By.className('test-file-upload-button'))
-             .sendKeys('//filesCtrl.js');
-        })
-        .then(function () {
-          return ptor.findElement(protractor.By.className('test-file-upload-button'))
-             .sendKeys('//filesDirective.js');
-        })
-        .then(function () {
-          return ptor.findElement(protractor.By.className('test-file-upload-button'))
-             .sendKeys('//filesModalCtrl.js');
-        })
-        .then(function () {
-          return ptor.findElement(protractor.By.className('btn-primary')).click();
-        })
-        .then(function () {
-          return ptor.findElement(protractor.By.className('test-icon-stop')).click();
-        })
-        .then(function () {
-          return ptor.wait(function () {
-            return ptor.findElements(protractor.By.className('modal-body'))
-              .then(function (elements) {
-                return _.keys(elements).length === 0;
-              });
-          }, 5000);
-        })
-        .then(function () {
-          filesDirectiveSpan.getText().then(function (value) {
-            expect(value).toEqual('2 прикачени файла.');
-          });
+      filesDir.openModal();
+      filesDir.addFile('//filesCtrl.js');
+      filesDir.addFile('//filesDirective.js');
+      filesDir.addFile('//filesModalCtrl.js');
+      filesDir.uploadFiles();
+      ptor.findElement(protractor.By.className('test-icon-stop')).click()
+      .then(function () {
+        return ptor.wait(function () {
+          return ptor.findElements(protractor.By.className('modal-body'))
+            .then(function (elements) {
+              return _.keys(elements).length === 0;
+            });
+        }, 5000);
+      })
+      .then(function () {
+        filesDir.get().then(function (value) {
+          expect(value).toEqual('2 прикачени файла.');
         });
+      });
     });
   });
 }(protractor, describe, beforeEach, it, expect, require));
