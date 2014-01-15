24968410751f55976e8f1296bb2c7cc44fee60bf
PankaLM <pan_lm@abv.bg>
2014-01-15 14:03:26 +0200

    Rewrite person addresses tests.

---
+++ Gva.Web/App/test/e2e/pageObjects/searchPersonAddressPO.js
@@ -0,0 +1,17 @@
+﻿/*global module, by, require*/
+(function (module, by, require) {
+  'use strict';
+
+  var ScDatatable = require('./directives/scDatatable');
+
+  function SearchPersonAddressPO(context) {
+    this.breadcrumb = context.findElement(by.xpath('//ul[@class="breadcrumb"]/li[last()]'));
+    this.datatable = new ScDatatable(context.findElement(by.css('div[ng-model="addresses"]')));
+    this.firstDeleteBtn = context
+      .findElement(by.css('tbody tr:first-child button[name=deleteBtn]'));
+    this.firstEditBtn = context
+      .findElement(by.css('tbody tr:first-child button[name=editBtn]'));
+  }
+
+  module.exports = SearchPersonAddressPO;
+}(module, by, require));
