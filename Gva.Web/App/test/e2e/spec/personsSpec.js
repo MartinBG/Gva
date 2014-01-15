503ac0c4ebb8d3c61c070bca22fc1397afdf3280
dimitar.kaludov <mitakyt@gmail.com>
2014-01-15 16:48:24 +0200

    Rework personsSpec to use page object

--- Gva.Web/App/test/e2e/spec/personsSpec.js
+++ Gva.Web/App/test/e2e/spec/personsSpec.js
@@ -1,107 +1,56 @@
-﻿/*global protractor, describe, beforeEach, it, expect*/
-(function (protractor, describe, beforeEach, it, expect) {
+﻿/*global protractor, describe, beforeEach, it, expect, require*/
+(function (protractor, describe, beforeEach, it, expect, require) {
   'use strict';
 
   describe('Persons search page', function () {
     var ptor = protractor.getInstance(),
-        searchBtn,
-        newPersonBtn,
-        addBtn;
+    Page = require('../pageObjects/personsPO'),
+    personsPage;
 
     beforeEach(function () {
       ptor.get('#/persons');
-
-      searchBtn = ptor.findElement(protractor.By.css('div[action=\'search()\'] > button'));
-      newPersonBtn = ptor.findElement(protractor.By.css('div[action=\'newPerson()\'] > button'));
-      addBtn = ptor.findElement(protractor.By.css('div[action=add] button'));
+      personsPage = new Page(ptor);
     });
 
     it('should display all persons', function () {
-      ptor
-        .findElements(protractor.By.datatable('persons').column('names'))
-        .then(function (elements) {
-          var namesPromise = protractor.promise.fullyResolved(elements.map(function (el) {
-            return el.getText();
-          }));
-
-          expect(namesPromise).toEqual([
-            'Иван Иванов Иванов',
-            'Атанас Иванов Иванов',
-            'Петър Петров Петров'
-          ]);
-        });
+      expect(personsPage.datatable.getColumns('names', 'lin', 'uin', 'age')).toEqual([
+        ['Иван Иванов Иванов', '11232', '7005159385', '43'],
+        ['Атанас Иванов Иванов', '12345', '7903245888', '34'],
+        ['Петър Петров Петров', '11111', '6904245664', '44']
+      ]);
     });
 
     it('should search by lin', function () {
-      ptor.findElement(protractor.By.css('div[name=lin] > input')).sendKeys('11111');
+      personsPage.searchForm.setFilter('lin', '11111');
+      personsPage.searchForm.clickButton('search');
 
-      searchBtn.click();
-      var names = ptor.findElement(protractor.By.datatable('persons').row(1).column('names'));
-      expect(names.getText()).toEqual('Петър Петров Петров');
+      personsPage = new Page(ptor);
+      expect(personsPage.datatable.getColumn('names')).toEqual(['Петър Петров Петров']);
     });
 
     it('should search by uin', function () {
-      ptor.findElement(protractor.By.css('div[name=uin] > input')).sendKeys('6904245664');
+      personsPage.searchForm.setFilter('uin', '6904245664');
+      personsPage.searchForm.clickButton('search');
 
-      searchBtn.click();
-      var names = ptor.findElement(protractor.By.datatable('persons').row(1).column('names'));
-      expect(names.getText()).toEqual('Петър Петров Петров');
+      personsPage = new Page(ptor);
+      expect(personsPage.datatable.getColumn('names')).toEqual(['Петър Петров Петров']);
     });
 
     it('should search by names', function () {
-      addBtn.click();
-      ptor.findElements(protractor.By.css('div[action=add] li')).then(function (filters) {
-        filters[0].click();
-      });
-      ptor.findElement(protractor.By.css('div[name=names] > input')).sendKeys('Петър');
+      personsPage.searchForm.addFilter('names');
+      personsPage.searchForm.setFilter('names', 'Петър');
+      personsPage.searchForm.clickButton('search');
 
-      searchBtn.click();
-      var names = ptor.findElement(protractor.By.datatable('persons').row(1).column('names'));
-      expect(names.getText()).toEqual('Петър Петров Петров');
-    });
-
-    it('should search by licences', function () {
-      addBtn.click();
-      ptor.findElements(protractor.By.css('div[action=add] li')).then(function (filters) {
-        filters[1].click();
-      });
-      ptor.findElement(protractor.By.css('div[name=licences] > input')).sendKeys('ATCL');
-
-      searchBtn.click();
-      var names = ptor.findElement(protractor.By.datatable('persons').row(1).column('names'));
-      expect(names.getText()).toEqual('Иван Иванов Иванов');
-    });
-
-    it('should search by ratings', function () {
-      addBtn.click();
-      ptor.findElements(protractor.By.css('div[action=add] li')).then(function (filters) {
-        filters[2].click();
-      });
-      ptor.findElement(protractor.By.css('div[name=ratings] > input')).sendKeys('A 300/310');
-
-      searchBtn.click();
-      var names = ptor.findElement(protractor.By.datatable('persons').row(1).column('names'));
-      expect(names.getText()).toEqual('Иван Иванов Иванов');
-    });
-
-    it('should search by organization', function () {
-      addBtn.click();
-      ptor.findElements(protractor.By.css('div[action=add] li')).then(function (filters) {
-        filters[3].click();
-      });
-      ptor.findElement(protractor.By.css('div[name=organization] > input')).sendKeys('Wizz Air');
-
-      searchBtn.click();
-      var names = ptor.findElement(protractor.By.datatable('persons').row(1).column('names'));
-      expect(names.getText()).toEqual('Атанас Иванов Иванов');
+      personsPage = new Page(ptor);
+      expect(personsPage.datatable.getColumn('names')).toEqual(['Петър Петров Петров']);
     });
 
     it('should redirect to new person page', function () {
-      newPersonBtn.click();
+      personsPage.searchForm.clickButton('newPerson');
       ptor.getCurrentUrl().then(function (url) {
         expect(url).toEqual('http://localhost:52560/#/persons/new');
       });
     });
 
   });
-}(protractor, describe, beforeEach, it, expect));
+}(protractor, describe, beforeEach, it, expect, require));
