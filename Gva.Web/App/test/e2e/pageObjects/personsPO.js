85dd4bd45d597a06a68c0eef1a848cf714647f73
dimitar.kaludov <mitakyt@gmail.com>
2014-01-15 16:48:07 +0200

    Add persons page object

---
+++ Gva.Web/App/test/e2e/pageObjects/personsPO.js
@@ -0,0 +1,14 @@
+﻿/*global module, by, require*/
+(function (module, by, require){
+  'use strict';
+
+  var ScDatatable = require('./directives/scDatatable'),
+      ScSearch = require('./directives/scSearch');
+
+  function PersonsPO(context) {
+    this.datatable = new ScDatatable(context.findElement(by.css('div[ng-model="persons"]')));
+    this.searchForm = new ScSearch(context.findElement(by.tagName('form')), context);
+  }
+
+  module.exports = PersonsPO;
+}(module, by, require));
