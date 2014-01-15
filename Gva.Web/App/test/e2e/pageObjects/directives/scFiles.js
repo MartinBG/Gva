82e59e2f94f59db7fa29c4043d09bdb7ecc30441
dimitar.kaludov <mitakyt@gmail.com>
2014-01-15 16:16:52 +0200

    Add page object for scFiles

---
+++ Gva.Web/App/test/e2e/pageObjects/directives/scFiles.js
@@ -0,0 +1,31 @@
+﻿/*global module, by*/
+(function (module, by){
+  'use strict';
+
+  function ScFiles(element, context) {
+    this.element = element;
+    this.context = context;
+  }
+
+  ScFiles.prototype.get = function () {
+    return this.element.findElement(by.className('form-control')).getText();
+  };
+
+  ScFiles.prototype.openModal = function () {
+    this.element.findElement(by.className('glyphicon-file')).click();
+  };
+
+  ScFiles.prototype.closeModal = function () {
+    this.context.findElement(by.className('test-button-close')).click();
+  };
+
+  ScFiles.prototype.addFile = function (text) {
+    this.context.findElement(by.className('test-file-upload-button')).sendKeys(text);
+  };
+
+  ScFiles.prototype.uploadFiles = function () {
+    this.context.findElement(by.className('btn-primary')).click();
+  };
+
+  module.exports = ScFiles;
+}(module, by));
