379ad967309be2df2729c9d320cbc38a5d8b62ab
Albena Kumanova <akumanova@yahoo.com>
2014-01-16 11:51:29 +0200

    Rewrited sc-promise-state tests

---
+++ Gva.Web/App/test/e2e/pageObjects/directives/scPromiseState.js
@@ -0,0 +1,36 @@
+﻿/*global module*/
+(function (module) {
+  'use strict';
+
+  function ScPromiseState(element) {
+    this.element = element;
+  }
+
+  ScPromiseState.prototype.title = function () {
+    return this.element.getAttribute('title');
+  };
+
+  ScPromiseState.prototype.isActive = function () {
+    return this.element.isDisplayed();
+  };
+
+  ScPromiseState.prototype.isPending = function () {
+    return this.element.getAttribute('class').then(function (className) {
+      return className.split(' ').indexOf('glyphicon-loading') !== -1;
+    });
+  };
+
+  ScPromiseState.prototype.isResolved = function () {
+    return this.element.getAttribute('class').then(function (className) {
+      return className.split(' ').indexOf('glyphicon-ok') !== -1;
+    });
+  };
+
+  ScPromiseState.prototype.isRejected = function () {
+    return this.element.getAttribute('class').then(function (className) {
+      return className.split(' ').indexOf('glyphicon-ban-circle') !== -1;
+    });
+  };
+
+  module.exports = ScPromiseState;
+}(module));
