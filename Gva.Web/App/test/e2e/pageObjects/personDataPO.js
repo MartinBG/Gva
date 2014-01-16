2b699d81adfc32113e5bd4ede1a854286c0d7909
Nasko <samodatepipna@abv.bg>
2014-01-16 14:52:54 +0200

    Rewrite edit person data and add person data paje object

---
+++ Gva.Web/App/test/e2e/pageObjects/personDataPO.js
@@ -0,0 +1,44 @@
+﻿/*global module, by, require*/
+(function (module, by, require) {
+  'use strict';
+
+  var ScText = require('./directives/scText'),
+      ScNomenclature = require('./directives/scNomenclature'),
+      ScDate = require('./directives/scDate');
+
+  function PersonDataPO(context) {
+    
+    this.firstName = new ScText(context.findElement(by.input('model.firstName')));
+    this.middleName = new ScText(context.findElement(by.input('model.middleName')));
+    this.lastName = new ScText(context.findElement(by.input('model.lastName')));
+    this.firstNameAlt = new ScText(context.findElement(by.input('model.firstNameAlt')));
+    this.middleNameAlt = new ScText(context.findElement(by.input('model.middleNameAlt')));
+    this.lastNameAlt = new ScText(context.findElement(by.input('model.lastNameAlt')));
+    this.lin = new ScText(context.findElement(by.input('model.lin')));
+    this.uin = new ScText(context.findElement(by.input('model.uin')));
+    this.sex = new ScNomenclature('model.sex', context);
+    this.dateOfBirth = new ScDate(context.findElement(by.css('div[name=dateOfBirth]')), context);
+    this.placeOfBirth = new ScNomenclature('model.placeOfBirth', context);
+    this.country = new ScNomenclature('model.country', context);
+    this.email = new ScText(context.findElement(by.input('model.email')));
+    this.fax = new ScText(context.findElement(by.input('model.fax')));
+    this.phone1 = new ScText(context.findElement(by.input('model.phone1')));
+    this.phone2 = new ScText(context.findElement(by.input('model.phone2')));
+    this.phone3 = new ScText(context.findElement(by.input('model.phone3')));
+    this.phone4 = new ScText(context.findElement(by.input('model.phone4')));
+    this.phone5 = new ScText(context.findElement(by.input('model.phone5')));
+
+    this.saveBtn = context.findElement(by.name('saveBtn'));
+    this.cancelBtn = context.findElement(by.name('cancelBtn'));
+  }
+
+  PersonDataPO.prototype.save = function () {
+    this.saveBtn.click();
+  };
+
+  PersonDataPO.prototype.cancel = function () {
+    this.cancelBtn.click();
+  };
+
+  module.exports = PersonDataPO;
+}(module, by, require));
