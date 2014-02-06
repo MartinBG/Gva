e033e6b01ccac7dea2da1c7a9ba9a248ddbc703c
Nasko <samodatepipna@abv.bg>
2014-02-06 15:57:18 +0200

    Remove ambiguos error text and make hasError diretive lines more readable

--- Gva.Web/App/js/gva/directives/hasError/hasErrorDirective.js
+++ Gva.Web/App/js/gva/directives/hasError/hasErrorDirective.js
@@ -8,18 +8,19 @@
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
-        scope.$watchCollection('[form["' + attrs.gvaHasError +
-          '"].$invalid, form.$validated]', function (newValue, oldValue) {
-          if (newValue === oldValue) {
-            return;
+        scope.$watchCollection('[form["' + attrs.gvaHasError + '"].$invalid, form.$validated]',
+          function (newValue, oldValue) {
+            if (newValue === oldValue) {
+              return;
+            }
+            if (newValue[0] && newValue[1]) {
+              element.addClass('has-error');
+            }
+            else {
+              element.removeClass('has-error');
+            }
           }
-          if (newValue[0] && newValue[1]) {
-            element.addClass('has-error');
-          }
-          else {
-            element.removeClass('has-error');
-          }
-        });
+        );
       }
     };
   }
