06d798eeea8728034116632aacbf6d31546f6651
Nasko <samodatepipna@abv.bg>
2014-02-06 15:08:35 +0200

    Implement hasError directive

---
+++ Gva.Web/App/js/gva/directives/hasError/hasErrorDirective.js
@@ -0,0 +1,29 @@
+﻿// Usage: <div gva-has-error="fieldName"></div>
+/*global angular*/
+(function (angular) {
+  'use strict';
+
+  function HasErrorDirective() {
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        scope.$watchCollection('[form["' + attrs.gvaHasError +
+          '"].$invalid, form.$validated]', function (newValue, oldValue) {
+          if (newValue === oldValue) {
+            return;
+          }
+          if (newValue[0] && newValue[1]) {
+            element.addClass('has-error');
+          }
+          else {
+            element.removeClass('has-error');
+          }
+        });
+      }
+    };
+  }
+
+  angular.module('gva')
+    .directive('gvaHasError', HasErrorDirective);
+}(angular));
