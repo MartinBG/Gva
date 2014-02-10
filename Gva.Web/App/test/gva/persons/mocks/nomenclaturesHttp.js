872fd77ef699b3a93ec5e3c2483ed365be7eaa09
Albena Kumanova <akumanova@yahoo.com>
2014-02-10 17:36:59 +0200

    Fix nomenclatures

--- Gva.Web/App/test/gva/persons/mocks/nomenclaturesHttp.js
+++ Gva.Web/App/test/gva/persons/mocks/nomenclaturesHttp.js
@@ -5,9 +5,84 @@
 
   angular.module('app').config(function ($httpBackendConfiguratorProvider) {
     $httpBackendConfiguratorProvider
+      .when('GET', '/api/nomenclatures/personCheckDocumentRoles?term&id',
+        function ($params, $filter) {
+          var res = nomenclatures.personCheckDocumentRoles;
+
+          if ($params.id) {
+            res = $filter('filter')(res, { nomTypeValueId: parseInt($params.id, 10) }, true)[0];
+          } else {
+            if ($params.term) {
+              res = $filter('filter')(res, { name: $params.term }, false);
+            }
+          }
+
+          return [200, res];
+        })
+      .when('GET', '/api/nomenclatures/personCheckDocumentTypes?term&id',
+        function ($params, $filter) {
+          var res = nomenclatures.personCheckDocumentTypes;
+
+          if ($params.id) {
+            res = $filter('filter')(res, { nomTypeValueId: parseInt($params.id, 10) }, true)[0];
+          } else {
+            if ($params.term) {
+              res = $filter('filter')(res, { name: $params.term }, false);
+            }
+          }
+
+          return [200, res];
+        })
+      .when('GET', '/api/nomenclatures/documentTrainingRoles?term&id',
+        function ($params, $filter) {
+          var res = $filter('filter')(
+             nomenclatures.documentRoles,
+            function (nom) {
+              return nom.content.categoryCode === 'O';
+            },
+            true);
+
+          if ($params.id) {
+            res = $filter('filter')(res, { nomTypeValueId: parseInt($params.id, 10) }, true)[0];
+          } else {
+            if ($params.term) {
+              res = $filter('filter')(res, { name: $params.term }, false);
+            }
+          }
+
+          return [200, res];
+        })
+      .when('GET', '/api/nomenclatures/documentTrainingTypes?term&id',
+        function ($params, $filter) {
+          var res = nomenclatures.personOtherDocumentTypes;
+
+          if ($params.id) {
+            res = $filter('filter')(res, { nomTypeValueId: parseInt($params.id, 10) }, true)[0];
+          } else {
+            if ($params.term) {
+              res = $filter('filter')(res, { name: $params.term }, false);
+            }
+          }
+
+          return [200, res];
+        })
+      .when('GET', '/api/nomenclatures/personIdDocumentTypes?term&id',
+        function ($params, $filter) {
+          var res = nomenclatures.personIdDocumentTypes;
+
+          if ($params.id) {
+            res = $filter('filter')(res, { nomTypeValueId: parseInt($params.id, 10) }, true)[0];
+          } else {
+            if ($params.term) {
+              res = $filter('filter')(res, { name: $params.term }, false);
+            }
+          }
+
+          return [200, res];
+        })
       .when('GET',
           '/api/nomenclatures/:' +
-          'alias?term&parentId&type&id&staffTypeId&parentAlias&nomTypeParentValueId&categoryCode',
+          'alias?term&parentId&type&id&staffTypeId&parentAlias&nomTypeParentValueId',
         function ($params, $filter) {
           var res = nomenclatures[$params.alias];
 
@@ -26,15 +101,6 @@
               res = $filter('filter')(
                 res,
                 { nomTypeParentValueId: parseInt($params.nomTypeParentValueId, 10) },
-                true);
-            }
-
-            if ($params.categoryCode) {
-              res = $filter('filter')(
-                res,
-                function (nom) {
-                  return nom.content.categoryCode === $params.categoryCode;
-                },
                 true);
             }
 
