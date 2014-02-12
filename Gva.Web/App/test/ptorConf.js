f58145d92dbc5a309d31da9f8c78970a17becac6
Georgi Yordanov <georgi.yordanov@gmail.com>
2014-02-12 22:47:08 +0200

    Reorganize persons views

--- Gva.Web/App/test/ptorConf.js
+++ Gva.Web/App/test/ptorConf.js
@@ -5,11 +5,10 @@
   exports.config = {
     seleniumServerJar: process.env.SELENIUM_PATH + 'selenium-server-standalone-2.39.0.jar',
     specs: [
-      //'test/common/specs/*.js',
-      //'test/gva/persons/specs/*.js',
-      //'test/gva/directives/specs/*.js',
-      //'test/scaffolding/specs/*.js'
-      'test/gva/persons/specs/editPersonDataSpec.js'
+      'test/common/specs/*.js',
+      'test/gva/persons/specs/*.js',
+      'test/gva/directives/specs/*.js',
+      'test/scaffolding/specs/*.js'
     ],
     baseUrl: 'http://localhost:52560',
     rootElement: 'body',
