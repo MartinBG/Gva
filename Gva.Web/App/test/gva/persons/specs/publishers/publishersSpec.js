a4df0ee8ffb6ec11b7c18760fb74a4972d8a3e64
dimitar.kaludov <mitakyt@gmail.com>
2014-02-21 16:34:44 +0200

    Add choose publisher tests

---
+++ Gva.Web/App/test/gva/persons/specs/publishers/publishersSpec.js
@@ -0,0 +1,58 @@
+﻿/*global protractor, describe, beforeEach, it, expect, require*/
+(function (protractor, describe, beforeEach, it, expect, require) {
+  'use strict';
+
+  describe('Choose publishers page', function () {
+    var ptor = protractor.getInstance(),
+    Page = require('../../pageObjects/publishers/publishersPO'),
+    ParentPage = require('../../pageObjects/documentTrainings/trainingPO'),
+    publishersPage,
+    parentPage;
+
+    beforeEach(function () {
+      ptor.get('#/persons/1/documentTrainings/new/choosepublisher');
+      publishersPage = new Page(ptor);
+    });
+
+    it('should display publishers', function () {
+      expect(publishersPage.datatable.getColumns('name')).toEqual([
+        ['AAK Progres'],
+        ['Wizz Air'],
+        ['Fly Emirates'],
+        ['Training Flight Operations Support and Services'],
+        ['Bombardier Aerospace Training Center'],
+        ['Поделение'],
+        ['SOFIA FLIGHT TRAINING CENTER'],
+        ['МВР'],
+        ['ST Aerospase'],
+        ['Japan Civil Aviation Authority']
+      ]);
+    });
+
+    it('should search', function () {
+      publishersPage.searchForm.setFilter('text', 'AAK');
+      publishersPage.searchForm.clickButton('search');
+
+      publishersPage = new Page(ptor);
+      expect(publishersPage.datatable.getColumn('name')).toEqual(['AAK Progres']);
+    });
+
+    it('should redirect to new person page', function () {
+      publishersPage.searchForm.clickButton('goBack');
+      ptor.getCurrentUrl().then(function (url) {
+        expect(url).toEqual('http://localhost:52560/#/persons/1/documentTrainings/new');
+      });
+    });
+
+    it('should select publisher and pass it to parent', function () {
+      publishersPage.firstSelectBtn.click();
+      expect(ptor.getCurrentUrl())
+        .toEqual('http://localhost:52560/#/persons/1/documentTrainings/new');
+      parentPage = new ParentPage(ptor);
+      parentPage.staffType.set('Общ документ');
+      expect(parentPage.documentPublisher.get())
+        .toEqual('AAK Progres');
+    });
+
+  });
+}(protractor, describe, beforeEach, it, expect, require));
