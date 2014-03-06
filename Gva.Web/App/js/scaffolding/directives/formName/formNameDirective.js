5a1608f686fd89bbe183b427d1b89b17ce27e7bc
Tsvetan Belchev <tsvetan.belchev@gmail.com>
2014-03-06 17:05:24 +0200

    Fix dynamic form creation; use angular way to get form controller;

---
+++ Gva.Web/App/js/scaffolding/directives/formName/formNameDirective.js
@@ -0,0 +1,50 @@
+﻿// Usage: <ng-form sc-form-name="<formNameExpr>"></div>
+/*global angular*/
+(function (angular) {
+  'use strict';
+
+  function FormNameDirective($parse) {
+    return {
+      restrict: 'A',
+      require: '?form',
+      link: function (scope, element, attrs, elementForm) {
+        var formName,
+            localName,
+            parentName,
+            parentForm;
+
+        if (!elementForm) {
+          return;
+        }
+
+        if (attrs.scFormName) {
+          formName = $parse(attrs.scFormName)(scope);
+          if (formName.local || formName.parent) {
+            localName = formName.local;
+            parentName = formName.parent;
+          } else {
+            parentName = formName;
+          }
+        }
+
+        if (localName) {
+          scope[localName] = elementForm;
+          element.on('$destroy', function () {
+            scope[localName] = undefined;
+          });
+        }
+
+        if (parentName && !elementForm.$name) {
+          parentForm = element.parent().controller('form');
+          elementForm.$name = parentName;
+          parentForm.$addControl(elementForm);
+        }
+      }
+    };
+  }
+
+  FormNameDirective.$inject = ['$parse'];
+
+  angular.module('scaffolding')
+    .directive('scFormName', FormNameDirective);
+}(angular));
