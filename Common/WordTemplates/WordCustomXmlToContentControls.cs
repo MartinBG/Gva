af47e603dcf2f5156b6aea75d366646bea7d7d17
Atanas Dzhiljanov <naskodzhil@gmail.com>
2014-03-25 13:39:59 +0200

    Add word templates support

---
+++ Common/WordTemplates/WordCustomXmlToContentControls.cs
@@ -0,0 +1,102 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.WordTemplates
+{
+    public class WordCustomXmlToContentControls
+    {
+
+        public static void Replace(Stream template, bool hasNestedControls)
+        {
+            if (hasNestedControls)
+            {
+                int replacedElements;
+                do
+                {
+                    replacedElements = ReplaceHelper(template, hasNestedControls);
+                }
+                while (replacedElements != 0);
+            }
+            else
+            {
+                ReplaceHelper(template, hasNestedControls);
+            }
+        }
+
+        public static int ReplaceHelper(Stream template, bool hasNestedControls)
+        {
+            int replacedElements = 0;
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(template, true))
+            {
+                MainDocumentPart main = doc.MainDocumentPart;
+                var customXmlElements = main.Document.Descendants<CustomXmlElement>().Where(cb => !cb.Descendants<CustomXmlElement>().Any()).ToList();
+                foreach (var customXmlElement in customXmlElements)
+                {
+                    string elementXmlPath = String.Concat(customXmlElement.Ancestors<CustomXmlElement>().Select(ce => ce.GetAttribute("element", customXmlElement.NamespaceUri).Value + "/").ToArray().Reverse()),
+                           title = customXmlElement.GetAttribute("element", customXmlElement.NamespaceUri).Value,
+                           tagName;
+                    OpenXmlElement newElement = null;
+                    Random random = new Random();
+                    if(!hasNestedControls){
+                        tagName = elementXmlPath + customXmlElement.GetAttribute("element", customXmlElement.NamespaceUri).Value;
+                    }
+                    else{
+                        tagName = title;
+                    }
+                    int newId = random.Next(Int32.MinValue, Int32.MaxValue);
+
+                    SdtProperties sdtPr = new SdtProperties(
+                        new SdtAlias() { Val = title },
+                        new Tag() { Val = tagName },
+                        new SdtId() { Val = newId });
+
+                    if (customXmlElement.GetType() == typeof(CustomXmlBlock))
+                    {
+                        SdtContentBlock sdtContent = new SdtContentBlock();
+                        transferChildElements(customXmlElement, sdtContent);
+                        newElement = new SdtBlock(sdtPr, sdtContent);
+                    }
+                    else if (customXmlElement.GetType() == typeof(CustomXmlRun))
+                    {
+                        SdtContentRun sdtContent = new SdtContentRun();
+                        transferChildElements(customXmlElement, sdtContent);
+                        newElement = new SdtRun(sdtPr, sdtContent);
+                    }
+                    else if (customXmlElement.GetType() == typeof(CustomXmlRow))
+                    {
+                        SdtContentRow sdtContent = new SdtContentRow();
+                        transferChildElements(customXmlElement, sdtContent);
+                        newElement = new SdtRow(sdtPr, sdtContent);
+                    }
+                    else if (customXmlElement.GetType() == typeof(CustomXmlCell))
+                    {
+                        SdtContentCell sdtContent = new SdtContentCell();
+                        transferChildElements(customXmlElement, sdtContent);
+                        newElement = new SdtCell(sdtPr, sdtContent);
+                    }
+                    customXmlElement.Parent.InsertAfter(newElement, customXmlElement);
+                    customXmlElement.Remove();
+                }
+                replacedElements = customXmlElements.Count;
+            }
+            return replacedElements;
+        }
+
+        public static void transferChildElements(CustomXmlElement from, OpenXmlCompositeElement to)
+        {
+            OpenXmlElement[] elements = from.ChildElements.ToArray();
+            foreach (var e in elements)
+            {
+                e.Remove();
+            }
+            to.Append(elements);
+        }
+    }
+}
---
+++ Common/WordTemplates/WordTemplateProcessor.cs
@@ -0,0 +1,200 @@
+﻿using Common.Utils;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Text.RegularExpressions;
+
+namespace Common.WordTemplates
+{
+    public class WordTemplateTransformer
+    {
+        private Stream template;
+
+        public WordTemplateTransformer(Stream template)
+        {
+            this.template = template;
+        }
+
+        public Stream Template
+        {
+            get
+            {
+                return this.template;
+            }
+        }
+
+        public IEnumerable<T> ClosestDescendants<T>(OpenXmlElement root) where T : OpenXmlElement
+        {
+            foreach (var child in root.ChildElements)
+            {
+                if (child is T)
+                {
+                    yield return (T)child;
+                }
+                else
+                {
+                    foreach (var descendant in this.ClosestDescendants<T>(child))
+                    {
+                        yield return descendant;
+                    }
+                }
+            }
+        }
+
+        public void TransformElement(SdtElement sdtElement, JToken parentContext)
+        {
+            var sdtProperties = sdtElement.GetFirstChild<SdtProperties>();
+            var tag = sdtProperties != null ? sdtProperties.GetFirstChild<Tag>() : null;
+
+            if (tag == null)
+            {
+                return;
+            }
+            JToken context = null;
+            if (parentContext != null)
+            {
+                context = parentContext.SelectToken(tag.Val);
+            }
+
+            if (!sdtElement.Descendants<SdtElement>().Any())
+            {
+                if (sdtElement is SdtBlock)
+                {
+                    SdtBlock sdtBlock = (SdtBlock)sdtElement;
+                    Paragraph p = sdtBlock.SdtContentBlock.GetFirstChild<Paragraph>();
+                    Run r = p.GetFirstChild<Run>();
+                    setRunText(r, context);
+
+                    RemoveAllChildrenButFirst<Run>(p);
+                    RemoveAllChildrenButFirst<Paragraph>(sdtBlock.SdtContentBlock);
+                }
+                else if (sdtElement is SdtRun)
+                {
+                    SdtRun sdtRun = (SdtRun)sdtElement;
+                    Run r = sdtRun.SdtContentRun.GetFirstChild<Run>();
+                    setRunText(r, context);
+
+                    RemoveAllChildrenButFirst<Run>(sdtRun.SdtContentRun);
+                }
+                else if (sdtElement is SdtCell)
+                {
+                    SdtCell sdtCell = (SdtCell)sdtElement;
+                    TableCell cell = sdtCell.SdtContentCell.GetFirstChild<TableCell>();
+                    Paragraph p = cell.GetFirstChild<Paragraph>();
+                    Run r = p.GetFirstChild<Run>();
+                    setRunText(r, context);
+
+                    RemoveAllChildrenButFirst<Run>(p);
+                    RemoveAllChildrenButFirst<Paragraph>(cell);
+                }
+            }
+            else
+            {
+                if (context is JArray)
+                {
+                    OpenXmlElement insertAfter = sdtElement;
+                    foreach (var arrayItem in context)
+                    {
+                        var clone = sdtElement.CloneNode(true);
+                        sdtElement.Parent.InsertAfter(clone, insertAfter);
+                        insertAfter = clone;
+
+                        foreach (SdtElement childSdtElement in this.ClosestDescendants<SdtElement>(clone).ToList())
+                        {
+                            TransformElement(childSdtElement, arrayItem);
+                        }
+                    }
+
+                    sdtElement.Remove();
+                }
+                else
+                {
+                    foreach (SdtElement childSdtElement in this.ClosestDescendants<SdtElement>(sdtElement))
+                    {
+                        TransformElement(childSdtElement, context);
+                    }
+                }
+            }
+        }
+
+        public void Transform(JObject context)
+        {
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(template, true))
+            {
+                MainDocumentPart main = doc.MainDocumentPart;
+
+                foreach (var sdtElement in this.ClosestDescendants<SdtElement>(main.Document))
+                {
+                    this.TransformElement(sdtElement, context);
+                }
+
+                foreach (var sdtElement in main.Document.Descendants<SdtElement>().Reverse())
+                {
+                    this.RemoveContentControl(sdtElement);
+                }
+            }
+        }
+
+        public void RemoveContentControl(SdtElement sdtElement)
+        {
+            OpenXmlElement sdtContent = null;
+            if (sdtElement.GetFirstChild <SdtContentRun>() != null)
+            {
+                sdtContent = sdtElement.GetFirstChild<SdtContentRun>();
+            }
+            else if (sdtElement.GetFirstChild<SdtContentBlock>() != null)
+            {
+                sdtContent = sdtElement.GetFirstChild<SdtContentBlock>();
+            }
+            else if (sdtElement.GetFirstChild<SdtContentRow>() != null)
+            {
+                sdtContent = sdtElement.GetFirstChild<SdtContentRow>();
+            }
+            else if (sdtElement.GetFirstChild<SdtContentCell>() != null)
+            {
+                sdtContent = sdtElement.GetFirstChild<SdtContentCell>();
+            }
+            else
+            {
+                return;
+            }
+
+            OpenXmlElement parent = sdtElement.Parent;
+            OpenXmlElementList childElements = sdtContent.ChildElements;
+
+            foreach (OpenXmlElement childElement in childElements)
+            {
+                parent.InsertBefore((OpenXmlElement)childElement.Clone(), sdtElement);
+            }
+
+            sdtElement.Remove();
+        }
+
+        public void setRunText(Run r, JToken context)
+        {
+            Text runText = r.GetFirstChild<Text>();
+            runText.SetAttribute(new OpenXmlAttribute("space", XNamespace.Xml.NamespaceName, "preserve"));
+            runText.Text = context != null ? context.ToString() : string.Empty;
+        }
+
+        private void RemoveAllChildrenButFirst<T>(OpenXmlCompositeElement content) where T : OpenXmlElement
+        {
+            foreach (var e in content.Descendants<T>().Skip(1).Reverse())
+            {
+                e.Remove();
+            }
+        }
+    }
+}
