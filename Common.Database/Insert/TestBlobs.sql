ce892314d956e21e903d28395cf2213716539e66
Angel Yordanov <yordanov@gmail.com>
2014-04-13 18:50:53 +0300

    Add blob test data

---
+++ Common.Database/Insert/TestBlobs.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT [Blobs] ON
+
+INSERT INTO [Blobs]
+    ([BlobId], [Key]                                , [Hash]                                    , [Size], [Content] , [IsDeleted])
+VALUES
+    (1       ,'7C0604F9-FB44-4CCD-BE0E-66E82142AE76', 'A94A8FE5CCB19BA61C4C0873D391E987982FBBD3', 4     , 0x74657374, 0          )
+
+SET IDENTITY_INSERT [Blobs] OFF
+GO
