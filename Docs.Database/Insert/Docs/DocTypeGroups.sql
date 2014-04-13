3112a69c067312343409e94988cc31588656d753
Georgi Yordanov <georgi.yordanov@gmail.com>
2014-04-13 12:51:40 +0300

    Rename docs nomenclature

--- Docs.Database/Insert/Docs/DocTypeGroups.sql
+++ Docs.Database/Insert/Docs/DocTypeGroups.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO [DocTypeGroups]([DocTypeGroupId], [Name], [IsElectronicService], [IsActive]) VALUES(1,N'Системни',0,0);
 INSERT INTO [DocTypeGroups]([DocTypeGroupId], [Name], [IsElectronicService], [IsActive]) VALUES(100,N'Общи',0,1);
-INSERT INTO [DocTypeGroups]([DocTypeGroupId], [Name], [IsElectronicService], [IsActive]) VALUES(201,N'Искане',0,1);
+INSERT INTO [DocTypeGroups]([DocTypeGroupId], [Name], [IsElectronicService], [IsActive]) VALUES(201,N'Заявление',0,1);
 INSERT INTO [DocTypeGroups]([DocTypeGroupId], [Name], [IsElectronicService], [IsActive]) VALUES(202,N'Електронни услуги',1,1);
 INSERT INTO [DocTypeGroups]([DocTypeGroupId], [Name], [IsElectronicService], [IsActive]) VALUES(300,N'Отговори на услуги',1,1);
 INSERT INTO [DocTypeGroups]([DocTypeGroupId], [Name], [IsElectronicService], [IsActive]) VALUES(400,N'Невалидни услуги',1,0);
