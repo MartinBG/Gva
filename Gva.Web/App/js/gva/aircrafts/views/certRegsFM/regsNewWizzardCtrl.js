c794b158f73dfca7aaab9fb182db32626cddfe79
dimitar.kaludov <mitakyt@gmail.com>
2014-04-14 16:24:43 +0300

    Fix jshint errors

--- Gva.Web/App/js/gva/aircrafts/views/certRegsFM/regsNewWizzardCtrl.js
+++ Gva.Web/App/js/gva/aircrafts/views/certRegsFM/regsNewWizzardCtrl.js
@@ -26,32 +26,28 @@
           if ($scope.newRegForm.$valid) {
             $scope.newRegForm.$validated = false;
 
-
             switch ($scope.currentStep) {
-              case $scope.steps.chooseRegister:
-                return Aircraft.getNextCertNumber()
-                  .$promise.then(function (result) {
-                    $scope.model.certNumber = result.certNumber;
-                    $scope.currentStep = $scope.steps.chooseRegNumber;
-                  });
-                break;
-              case $scope.steps.chooseRegNumber:
-                $scope.currentStep = $scope.steps.chooseRegMark;
-                break;
-              case $scope.steps.chooseRegMark:
-                return Aircraft.checkRegMark({ regMark: $scope.model.regMark })
-                  .$promise.then(function (result) {
-                    if (result.isValid) {
-                      $scope.currentStep = $scope.steps.confirmRegMark;
-                    }
-                    else {
-                      $scope.currentStep = $scope.steps.regMarkInUse;
-                    }
-                  });
-                break;
-              case $scope.steps.confirmRegMark:
-                return $state.go('root.aircrafts.view.regsFM.new', {}, {}, $scope.model);
-                break;
+            case $scope.steps.chooseRegister:
+              return Aircraft.getNextCertNumber()
+                .$promise.then(function (result) {
+                  $scope.model.certNumber = result.certNumber;
+                  $scope.currentStep = $scope.steps.chooseRegNumber;
+                });
+            case $scope.steps.chooseRegNumber:
+              $scope.currentStep = $scope.steps.chooseRegMark;
+              break;
+            case $scope.steps.chooseRegMark:
+              return Aircraft.checkRegMark({ regMark: $scope.model.regMark })
+                .$promise.then(function (result) {
+                  if (result.isValid) {
+                    $scope.currentStep = $scope.steps.confirmRegMark;
+                  }
+                  else {
+                    $scope.currentStep = $scope.steps.regMarkInUse;
+                  }
+                });
+            case $scope.steps.confirmRegMark:
+              return $state.go('root.aircrafts.view.regsFM.new', {}, {}, $scope.model);
             }
           }
         });
@@ -59,18 +55,18 @@
 
     $scope.back = function () {
       switch ($scope.currentStep) {
-        case $scope.steps.chooseRegNumber:
-          $scope.currentStep = $scope.steps.chooseRegister;
-          break;
-        case $scope.steps.chooseRegMark:
-          $scope.currentStep = $scope.steps.chooseRegNumber;
-          break;
-        case $scope.steps.confirmRegMark:
-          $scope.currentStep = $scope.steps.chooseRegMark;
-          break;
-        case $scope.steps.regMarkInUse:
-          $scope.currentStep = $scope.steps.chooseRegMark;
-          break;
+      case $scope.steps.chooseRegNumber:
+        $scope.currentStep = $scope.steps.chooseRegister;
+        break;
+      case $scope.steps.chooseRegMark:
+        $scope.currentStep = $scope.steps.chooseRegNumber;
+        break;
+      case $scope.steps.confirmRegMark:
+        $scope.currentStep = $scope.steps.chooseRegMark;
+        break;
+      case $scope.steps.regMarkInUse:
+        $scope.currentStep = $scope.steps.chooseRegMark;
+        break;
       }
     };
 
