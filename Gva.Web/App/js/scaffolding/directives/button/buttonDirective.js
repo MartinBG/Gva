abe4ec5b6496099d7fc71b5b691564d3b1cc9608
Atanas Dzhiljanov <naskodzhil@gmail.com>
2014-05-09 12:06:50 +0300

    Fix sc-button directive to accept type attribute

--- Gva.Web/App/js/scaffolding/directives/button/buttonDirective.js
+++ Gva.Web/App/js/scaffolding/directives/button/buttonDirective.js
@@ -1,4 +1,4 @@
-﻿// Usage: <sc-button name="" btn-click="" text="" class="" icon=""></sc-button>
+﻿// Usage: <sc-button type="" name="" btn-click="" text="" class="" icon=""></sc-button>
 
 /*global angular*/
 (function (angular) {
@@ -6,6 +6,39 @@
 
   function ButtonDirective($parse, $exceptionHandler, l10n) {
 
+    function ButtonLink(scope, element, attrs) {
+      var elementCtrl = {};
+
+      scope.$parent[attrs.name] = elementCtrl;
+
+      element.on('click', function (event) {
+        if (elementCtrl.$pending) {
+          return;
+        }
+
+        scope.$apply(function () {
+          // add $event local variables to the expression context as ngClick does
+          var result = scope.btnClick({ $event: event });
+
+          // check if the result is promise
+          if (result && result.then && typeof (result.then) === 'function') {
+            scope.$pending = true;
+            elementCtrl.$pending = true;
+            result['catch'](function (error) {
+              $exceptionHandler(error);
+            })['finally'](function () {
+              delete elementCtrl.$pending;
+              delete scope.$pending;
+            });
+          }
+        });
+      });
+
+      scope.text = l10n.get(attrs.text);
+      if (!scope.text) {
+        scope.text = attrs.text;
+      }
+    }
     return {
       restrict: 'E',
       replace: true,
@@ -14,38 +47,12 @@
         icon: '@'
       },
       templateUrl: 'scaffolding/directives/button/buttonDirective.html',
-      link: function (scope, element, attrs) {
-        var elementCtrl = {};
-
-        scope.$parent[attrs.name] = elementCtrl;
-
-        element.on('click', function (event) {
-          if (elementCtrl.$pending) {
-            return;
-          }
-
-          scope.$apply(function () {
-            // add $event local variables to the expression context as ngClick does
-            var result = scope.btnClick({ $event: event });
-
-            // check if the result is promise
-            if (result && result.then && typeof (result.then) === 'function') {
-              scope.$pending = true;
-              elementCtrl.$pending = true;
-              result['catch'](function (error) {
-                $exceptionHandler(error);
-              })['finally'](function () {
-                delete elementCtrl.$pending;
-                delete scope.$pending;
-              });
-            }
-          });
-        });
-
-        scope.text = l10n.get(attrs.text);
-        if (!scope.text) {
-          scope.text = attrs.text;
+      compile: function compile(tElement, tAttrs) {
+        if(!tAttrs.type) {
+          tAttrs.type = 'button';
         }
+
+        return ButtonLink;
       }
     };
   }
