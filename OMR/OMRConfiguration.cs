5de4464783871fb7a7b8579c9781ad3d04702fd2
Tsvetan Belchev <tsvetan.belchev@gmail.com>
2014-05-19 11:20:19 +0300

    OMR Library add

---
+++ OMR/OMRConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMR
+{
+    public class OMRConfiguration
+    {
+        public List<OMRQuestionBlock> Blocks { get; set; }
+        public OMRAdjustmentBlock AdjustmentBlock { get; set; }
+        public OMRAdjustmentBlock WhiteBlock { get; set; }
+        public double DarkFactor { get; set; }
+        public double FillFactor { get; set; }
+        public int ImageWidth { get; set; }
+        public int ImageHeight { get; set; }
+
+        public OMRConfiguration()
+        {
+            this.Blocks = new List<OMRQuestionBlock>();
+        }
+    }
+}
