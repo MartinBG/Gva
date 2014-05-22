e355577854f9e5c7da5a5a624d1753a9b1294cdc
Tsvetan Belchev <tsvetan.belchev@gmail.com>
2014-05-22 10:24:21 +0300

    Add extract answers from pdf logic
    --Add GhostScript.NET from nuget

---
+++ Gva.Web.App/js/gva/persons/resources/personExamAnswers.js
@@ -0,0 +1,8 @@
+﻿/*global angular*/
+(function (angular) {
+  'use strict';
+
+  angular.module('gva').factory('PersonExamAnswers', ['$resource', function ($resource) {
+    return $resource('/api/persons/:id/examAnswers');
+  }]);
+}(angular));
--- Gva.Web.App/js/gva/persons/views/exams/examsNewCtrl.js
+++ Gva.Web.App/js/gva/persons/views/exams/examsNewCtrl.js
@@ -6,10 +6,15 @@
     $scope,
     $state,
     $stateParams,
+    PersonExamAnswers,
     PersonExam,
     exam
   ) {
     $scope.exam = exam;
+    $scope.messages = {
+      good: undefined,
+      bad: undefined
+    };
 
     $scope.save = function () {
       return $scope.newExamForm.$validate()
@@ -24,6 +29,39 @@
         });
     };
 
+    $scope.extractAnswers = function () {
+      if ($scope.exam.files.length > 0 && $scope.exam.files[0].file !== null) {
+        return PersonExamAnswers.get({
+          id: $stateParams.id,
+          fileKey: $scope.exam.files[0].file.key
+        }).$promise
+        .then(function (data) {
+          if (data.msg) {
+            $scope.messages.bad = data.msg;
+            $scope.messages.good = undefined;
+          }
+          else {
+            $scope.exam.part.commonQuestions1 = data.answ.commonQuestions1;
+            $scope.exam.part.commonQuestions2 = data.answ.commonQuestions2;
+            $scope.exam.part.specializedQuestions1 = data.answ.specializedQuestions1;
+            $scope.exam.part.specializedQuestions2 = data.answ.specializedQuestions2;
+
+            $scope.exam.part.file = 'data:image/jpg;base64,' + data.file;
+            $scope.messages.good = 'Успешно';
+            $scope.messages.bad = undefined;
+          }
+        });
+      }
+      else {
+        $scope.messages.bad = 'Моля, прикачете файл!';
+        $scope.messages.good = undefined;
+      }
+    };
+
+    $scope.gradeExam = function () {
+      
+    };
+
     $scope.cancel = function () {
       return $state.go('root.persons.view.examASs.search');
     };
@@ -33,6 +71,7 @@
     '$scope',
     '$state',
     '$stateParams',
+    'PersonExamAnswers',
     'PersonExam',
     'exam'
   ];
@@ -41,7 +80,21 @@
     exam: function () {
       return {
         part: {
-          commonQuestions: [
+          commonQuestions1: [
+            {},
+            {},
+            {},
+            {},
+            {}
+          ],
+          commonQuestions2: [
+            {},
+            {},
+            {},
+            {},
+            {}
+          ],
+          specializedQuestions1: [
             {},
             {},
             {},
@@ -53,17 +106,7 @@
             {},
             {}
           ],
-          specializedQuestions: [
-            {},
-            {},
-            {},
-            {},
-            {},
-            {},
-            {},
-            {},
-            {},
-            {},
+          specializedQuestions2: [
             {},
             {},
             {},
