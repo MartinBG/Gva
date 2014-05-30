abd1a28ebd743d20c949c2590c9c24bdf3b81d14
Yanislav Galyov <y.galyov@gmail.com>
2014-05-30 11:59:36 +0300

    $(Aop) fixed aop states

--- Aop.Web.App/js/aop/aop.js
+++ Aop.Web.App/js/aop/aop.js
@@ -24,7 +24,7 @@
       .state(['root.apps'                                     , '/aop/app?limit&offset'                                                                                                                                                                                    ])
       .state(['root.apps.search'                              , ''                                                                                                     , ['@root'           , 'js/aop/views/appSearch.html'                         ,'AppsSearchCtrl'        ]])
       .state(['root.apps.edit'                                , '/:id'                                                                                                 , ['@root'           , 'js/aop/views/appEdit.html'                           ,'AppsEditCtrl'          ]])
-      .state(['root.apps.edit.docSelect'                      , '/caseSelect?csFromDate&csToDate&csRegUri&csDocName&csDocTypeId&csDocStatusId&csCorrs&csUnits&csIsCase', ['@root'           , 'js/docs/views/caseSelect.html'                       ,'AppDocSelectCtrl'      ]])
+      .state(['root.apps.edit.docSelect'                      , '/caseSelect?csFromDate&csToDate&csRegUri&csDocName&csDocTypeId&csDocStatusId&csCorrs&csUnits&csIsCase', ['@root'           , 'js/ems/docs/views/caseSelect.html'                       ,'AppDocSelectCtrl'      ]])
       .state(['root.apps.edit.newAopEmployer'                 , '/newAopEmployer'                                                                                      , ['@root'           , 'js/aop/views/appEmployerNew.html'                    ,'AppEmployerNewCtrl'    ]]);
   }]);
 }(angular));
