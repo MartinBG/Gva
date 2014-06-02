90e521c5bd9269278e0489bab1a020c0e36f70ca
Atanas Dzhiljanov <naskodzhil@gmail.com>
2014-06-02 15:43:54 +0300

    $(Common)Implement formReadonly directive

---
+++ Common.Web.App/js/scaffolding/directives/formReadonly/formReadonlyDirective.js
@@ -0,0 +1,46 @@
+﻿// Usage: <ng-form name="" sc-form-readonly=""></ng-form>
+
+/*global angular,_*/
+(function (angular, _) {
+  'use strict';
+
+  function FormReadonlyDirective() {
+    return {
+      restrict: 'A',
+      require: ['?form'],
+      scope: false,
+      link: function (scope, element, attrs, formController) {
+        var parentForm = _.first(_.map(element.parents('ng-form, [ng-form]'), function (formElem) {
+              return angular.element(formElem).controller('form');
+            })),
+            deregFunc,
+            form = formController[0];
+
+        scope.$watch(attrs.scFormReadonly, function (formReadonly) {
+          if (formReadonly !== undefined) {
+            form.$readonly = formReadonly;
+          }
+          else {
+            if (parentForm && _.has(parentForm, '$readonly')) {
+              if (deregFunc) {
+                deregFunc();
+              }
+              deregFunc = scope.$watch(
+                function () {
+                  return parentForm.$readonly;
+                },
+                function (formReadonly) {
+                  form.$readonly = formReadonly;
+              });
+            }
+            else {
+              form.$readonly = false;
+            }
+          }
+        });
+      }
+    };
+  }
+
+  angular.module('scaffolding').directive('scFormReadonly', FormReadonlyDirective);
+}(angular, _));
