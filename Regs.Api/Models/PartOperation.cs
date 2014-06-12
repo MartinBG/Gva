981405f6de568dec6eec6a5b135c8318559090df
Angel Yordanov <yordanov@gmail.com>
2014-06-12 14:19:07 +0300

    $(Regs) Remove unused enum descriptions

--- Regs.Api/Models/PartOperation.cs
+++ Regs.Api/Models/PartOperation.cs
@@ -4,13 +4,8 @@ namespace Regs.Api.Models
 {
     public enum PartOperation
     {
-        [Description("Добавяне")]
         Add = 1,
-
-        [Description("Изтриване")]
         Delete,
-
-        [Description("Обновяване")]
         Update
     }
 }
