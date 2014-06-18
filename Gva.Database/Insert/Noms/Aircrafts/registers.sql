8ded28c0bc0c1e60994d068639900c98bb373da9
Angel Yordanov <yordanov@gmail.com>
2014-06-18 12:07:49 +0300

    $(Gva) Add aircraft registers aliases

--- Gva.Database/Insert/Noms/Aircrafts/registers.sql
+++ Gva.Database/Insert/Noms/Aircrafts/registers.sql
@@ -4,8 +4,8 @@ INSERT [dbo].[Noms] ([Name], [Alias]) VALUES (N'Регистър ВС', N'regist
 DECLARE @nomId INT = @@IDENTITY
 
 INSERT INTO [NomValues]
-    ([NomId], [Code], [Name]       , [NameAlt]    , [ParentValueId], [Alias], [IsActive], [TextContent]         )
+    ([NomId], [Code], [Name]       , [NameAlt]    , [ParentValueId], [Alias]     , [IsActive], [TextContent]         )
 VALUES
-    (@nomId , N'1'  , N'Регистър 1', N'Register 1', NULL           , NULL   , 1         , N'{"regPrefix":""}'   ),
-    (@nomId , N'2'  , N'Регистър 2', N'Register 2', NULL           , NULL   , 1         , N'{"regPrefix":"II-"}')
+    (@nomId , N'1'  , N'Регистър 1', N'Register 1', NULL           , N'register1', 1         , N'{"regPrefix":""}'   ),
+    (@nomId , N'2'  , N'Регистър 2', N'Register 2', NULL           , N'register2', 1         , N'{"regPrefix":"II-"}')
 GO
