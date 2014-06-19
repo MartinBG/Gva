d88d5a9a91e4c99b410902c212c1ec7f272f5ef9
Angel Yordanov <yordanov@gmail.com>
2014-06-19 16:01:27 +0300

    $(Gva) Fix inspectorTypes alignment

--- Gva.Database/Insert/Noms/Aircrafts/inspectorTypes.sql
+++ Gva.Database/Insert/Noms/Aircrafts/inspectorTypes.sql
@@ -4,9 +4,9 @@ INSERT [dbo].[Noms] ([Name], [Alias]) VALUES (N'Тип инспектор', N'in
 DECLARE @nomId INT = @@IDENTITY
 
 INSERT INTO [NomValues]
-    ([NomId], [Code], [Name]       , [NameAlt]    , [ParentValueId], [Alias]     , [IsActive])
+    ([NomId], [Code], [Name]          , [NameAlt]       , [ParentValueId], [Alias]        , [IsActive])
 VALUES
     (@nomId , N'I'  , N'Инспектор ГВА', N'Inspector CAA', NULL           , N'gvaInspector', 1         ),
-    (@nomId , N'E'  , N'Проверяващ', N'Examiner', NULL           , N'examiner', 1         ),
-    (@nomId , N'O'  , N'Друг', N'Other', NULL           , N'other', 1         )
+    (@nomId , N'E'  , N'Проверяващ'   , N'Examiner'     , NULL           , N'examiner'    , 1         ),
+    (@nomId , N'O'  , N'Друг'         , N'Other'        , NULL           , N'other'       , 1         )
 GO
