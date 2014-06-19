9913e6b872d8d7329665aba1f8898f8c5aa2fcf4
dimitar.kaludov <mitakyt@gmail.com>
2014-06-19 15:06:36 +0300

    $(Gva) fix airworthiness nomenclatures

--- Gva.Database/Insert/Noms/Aircrafts/airworthinessCertificateTypes.sql
+++ Gva.Database/Insert/Noms/Aircrafts/airworthinessCertificateTypes.sql
@@ -4,9 +4,11 @@ INSERT INTO Noms (Name, Alias) VALUES ('AC cert type','airworthinessCertificateT
 DECLARE @nomId INT = @@IDENTITY
 
 INSERT INTO [NomValues]
-    ([NomId], [Code], [Name]                                                , [NameAlt]         , [ParentValueId], [Alias], [IsActive], [TextContent])
+    ([NomId], [Code], [Name]                                                , [NameAlt]                                  , [ParentValueId], [Alias]     , [IsActive], [TextContent])
 VALUES
-    (@nomId , N'AC' , N'Удостоверение за летателна годност (F25)'           , N'Experimental'   , NULL           , NULL   , 1         , NULL         ),
-    (@nomId , N'AR' , N'Ограничено удостоверение за летателна годност (F24)', N'Gyroplane'      , NULL           , NULL   , 1         , NULL         ),
-    (@nomId , N'AS' , N'Специално удостоверение за летателна годност'       , N'Motor-hanglider', NULL           , NULL   , 1         , NULL         )
+    (@nomId , N'AC' , N'Удостоверение за летателна годност (F25)'           , N'Airworthiness certificate (F25)'         , NULL           , 'f25'       , 1         , NULL         ),
+    (@nomId , N'AR' , N'Ограничено удостоверение за летателна годност (F24)', N'Limited airworthiness certificate (F24)' , NULL           , 'f24'       , 1         , NULL         ),
+    (@nomId , N'AS' , N'Специално удостоверение за летателна годност'       , N'Special airworthiness certificate'       , NULL           , 'special'   , 1         , NULL         ),
+    (@nomId , N'A9' , N'Удостоверение за летателна годност (Наредба 8)'     , N'Airworthiness certificate (Directive 8)' , NULL           , 'directive8', 1         , NULL         ),
+    (@nomId , N'AU' , N'Удостоверение за летателна годност (СлВС)'          , N'Airworthiness certificate (ULAC)'        , NULL           , 'ulac'      , 1         , NULL         )
 GO
--- Gva.Database/Insert/Noms/Aircrafts/airworthinessReviewTypes.sql
+++ Gva.Database/Insert/Noms/Aircrafts/airworthinessReviewTypes.sql
@@ -4,8 +4,8 @@ INSERT INTO Noms (Name, Alias) VALUES ('AC cert type','airworthinessReviewTypes'
 DECLARE @nomId INT = @@IDENTITY
 
 INSERT INTO [NomValues]
-    ([NomId], [Code], [Name]                                 , [NameAlt]               , [ParentValueId], [Alias], [IsActive], [TextContent])
+    ([NomId], [Code], [Name]                                 , [NameAlt]                                , [ParentValueId], [Alias], [IsActive], [TextContent])
 VALUES
-    (@nomId , N'AV' , N'Удостоверение за преглед за ЛГ (15a)', N'Paramotor-Trike'      , NULL           , NULL   , 1         , NULL         ),
-    (@nomId , N'AW' , N'Удостоверение за преглед за ЛГ (15b)', N'Very Light Rotorcraft', NULL           , NULL   , 1         , NULL         )
+    (@nomId , N'AV' , N'Удостоверение за преглед за ЛГ (15a)', N'Airworthiness review certificate (15a)', NULL           , '15a'  , 1         , NULL         ),
+    (@nomId , N'AW' , N'Удостоверение за преглед за ЛГ (15b)', N'Airworthiness review certificate (15b)', NULL           , '15b'  , 1         , NULL         )
 GO
