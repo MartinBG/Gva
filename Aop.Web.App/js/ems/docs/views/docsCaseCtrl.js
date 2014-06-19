98d7a2e9e5987901e6d96569dd81192fb0726a9e
PankaLM <pan_lm@abv.bg>
2014-06-19 17:31:13 +0300

    $(Aop) Change docsCase view to use treetable directive.

--- Aop.Web.App/js/ems/docs/views/docsCaseCtrl.js
+++ Aop.Web.App/js/ems/docs/views/docsCaseCtrl.js
@@ -10,7 +10,7 @@
   ) {
     $scope.aopAppId = doc.aopAppId;
     $scope.docId = doc.docId;
-
+    
     $scope.isCase = doc.isCase;
 
     $scope.aopApplicationId = undefined;
@@ -21,7 +21,32 @@
       $scope.aopApplicationId = result.aopApplicationId;
     });
 
-    $scope.docRelations = _.cloneDeep(doc.docRelations);
+    $scope.docRelations = [];
+    $scope.relations = _.cloneDeep(doc.docRelations);
+
+    var index = _.findIndex($scope.relations, { 'parentDocId': undefined }),
+      parentDoc = $scope.relations.splice(index, 1)[0];
+    parentDoc.rowId = 1;
+    $scope.docRelations.push(parentDoc);
+
+    function buildHierarchy(currentParent){
+      var childId = 1;
+      var children = _.where($scope.relations, {'parentDocId': currentParent.docId});
+
+      _.forEach(children, function(docRelation){
+        docRelation.parentId = currentParent.rowId;
+        docRelation.rowId =  docRelation.parentId + '.' + childId++;
+
+        $scope.docRelations.push(docRelation);
+        $scope.relations.splice(_.findIndex($scope.relations, { 'docId': docRelation.docId }), 1);
+
+        if(_.where($scope.relations, {'parentDocId': docRelation.docId}).length > 0){
+          buildHierarchy(docRelation);
+        }
+      });
+    }
+
+    buildHierarchy(parentDoc);
 
     $scope.viewDoc = function (docId) {
       return $state.go('root.docs.edit.view', { id: docId });
