24eef21ef337a9f405ad283f33448db182dbff29
PankaLM <pan_lm@abv.bg>
2014-06-20 11:25:54 +0300

    $(Mosv, Gva, Common) Change all docCases to use treetable directive.

--- Common.Web.App/js/ems/docs/views/docsCaseCtrl.js
+++ Common.Web.App/js/ems/docs/views/docsCaseCtrl.js
@@ -13,7 +13,33 @@
     $scope.appId = application.id;
     $scope.docId = doc.docId;
     $scope.isCase = doc.isCase;
-    $scope.docRelations = _.cloneDeep(doc.docRelations);
+
+    $scope.docRelations = [];
+    $scope.relations = _.cloneDeep(doc.docRelations);
+
+    var index = _.findIndex($scope.relations, { 'parentDocId': undefined }),
+      parentDoc = $scope.relations.splice(index, 1)[0];
+    parentDoc.rowId = 1;
+    $scope.docRelations.push(parentDoc);
+
+    function buildHierarchy(currentParent){
+      var childId = 1;
+      var children = _.where($scope.relations, {'parentDocId': currentParent.docId});
+
+      _.forEach(children, function(docRelation){
+        docRelation.parentId = currentParent.rowId;
+        docRelation.rowId =  docRelation.parentId + '.' + childId++;
+
+        $scope.docRelations.push(docRelation);
+        $scope.relations.splice(_.findIndex($scope.relations, { 'docId': docRelation.docId }), 1);
+
+        if(_.where($scope.relations, {'parentDocId': docRelation.docId}).length > 0){
+          buildHierarchy(docRelation);
+        }
+      });
+    }
+
+    buildHierarchy(parentDoc);
 
     $scope.viewDoc = function (docId) {
       return $state.go('root.docs.edit.view', { id: docId });
