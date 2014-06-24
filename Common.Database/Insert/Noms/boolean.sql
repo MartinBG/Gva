d08a6ed7b1d190b46b640bea665f2d8cfa7c283a
Tsvetan Belchev <tsvetan.belchev@gmail.com>
2014-06-24 14:14:53 +0300

    $(Common) Change boolean nomenclature to right value names

--- Common.Database/Insert/Noms/boolean.sql
+++ Common.Database/Insert/Noms/boolean.sql
@@ -6,6 +6,6 @@ DECLARE @nomId INT = @@IDENTITY
 INSERT INTO [NomValues]
     ([NomId], [Code], [Name]         , [NameAlt], [ParentValueId], [Alias], [IsActive], [TextContent])
 VALUES
-    (@nomId , N'Y'  , N'Персонал'    , NULL     , NULL           , N'yes' , 1         , NULL         ),
-    (@nomId , N'N'  , N'Организация' , NULL     , NULL           , N'no'  , 1         , NULL         )
+    (@nomId , N'Y'  , N'Да'          , NULL     , NULL           , N'yes' , 1         , NULL         ),
+    (@nomId , N'N'  , N'Не'          , NULL     , NULL           , N'no'  , 1         , NULL         )
 GO
