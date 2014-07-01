9c3c226ec6f847626e01ece21f16eb369a5b6dcf
Angel Yordanov <yordanov@gmail.com>
2014-07-01 18:47:45 +0300

    $(Gva) Remove unused resource

--- Gva.Web.App/js/gva/aircrafts/resources/aircraftCurrentDebtFM.js
+++
@@ -1,8 +0,0 @@
-﻿/*global angular*/
-(function (angular) {
-  'use strict';
-
-  angular.module('gva').factory('AircraftCurrentDebtFM', ['$resource', function ($resource) {
-    return $resource('/api/aircrafts/:id/aircraftCurrentDocumentDebtsFM/:ind');
-  }]);
-}(angular));
