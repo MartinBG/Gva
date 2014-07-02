b699c62680d2a5d28ee7713210147e147e2ff5af
Yanislav Galyov <y.galyov@gmail.com>
2014-07-02 13:20:36 +0300

    $(Ems, Aop, Mosv) Fixed doc case view

--- Common.Web.App/js/ems/docs/views/docsCaseCtrl.js
+++ Common.Web.App/js/ems/docs/views/docsCaseCtrl.js
@@ -16,23 +16,25 @@
     $scope.docRelations = [];
     $scope.relations = _.cloneDeep(doc.docRelations);
 
-    var index = _.findIndex($scope.relations, { 'parentDocId': undefined }),
+    var index = _.findIndex($scope.relations, function (item) {
+      return item.parentDocId === undefined || item.parentDocId === null;
+    }),
       parentDoc = $scope.relations.splice(index, 1)[0];
     parentDoc.rowId = 1;
     $scope.docRelations.push(parentDoc);
 
-    function buildHierarchy(currentParent){
+    function buildHierarchy(currentParent) {
       var childId = 1;
-      var children = _.where($scope.relations, {'parentDocId': currentParent.docId});
+      var children = _.where($scope.relations, { 'parentDocId': currentParent.docId });
 
-      _.forEach(children, function(docRelation){
+      _.forEach(children, function (docRelation) {
         docRelation.parentId = currentParent.rowId;
-        docRelation.rowId =  docRelation.parentId + '.' + childId++;
+        docRelation.rowId = docRelation.parentId + '.' + childId++;
 
         $scope.docRelations.push(docRelation);
         $scope.relations.splice(_.findIndex($scope.relations, { 'docId': docRelation.docId }), 1);
 
-        if(_.where($scope.relations, {'parentDocId': docRelation.docId}).length > 0){
+        if (_.where($scope.relations, { 'parentDocId': docRelation.docId }).length > 0) {
           buildHierarchy(docRelation);
         }
       });
--- Mosv.Web.App/js/ems/docs/views/docsCaseCtrl.js
+++ Mosv.Web.App/js/ems/docs/views/docsCaseCtrl.js
@@ -5,44 +5,34 @@
   function DocsCaseCtrl(
     $scope,
     $state,
-    Aops,
     doc
   ) {
-    $scope.aopAppId = doc.aopAppId;
     $scope.docId = doc.docId;
 
     $scope.isCase = doc.isCase;
 
-    $scope.aopApplicationId = undefined;
-
-    Aops.findAopApp({
-      id: $scope.docId
-    }).$promise.then(function (result) {
-      $scope.aopApplicationId = result.aopApplicationId;
-    });
-
     $scope.docRelations = [];
     $scope.relations = _.cloneDeep(doc.docRelations);
 
-    var index = _.findIndex($scope.relations, function (item) {
+    var index = _.findIndex($scope.relations, function(item)  {
       return item.parentDocId === undefined || item.parentDocId === null;
     }),
       parentDoc = $scope.relations.splice(index, 1)[0];
     parentDoc.rowId = 1;
     $scope.docRelations.push(parentDoc);
 
-    function buildHierarchy(currentParent) {
+    function buildHierarchy(currentParent){
       var childId = 1;
-      var children = _.where($scope.relations, { 'parentDocId': currentParent.docId });
+      var children = _.where($scope.relations, {'parentDocId': currentParent.docId});
 
-      _.forEach(children, function (docRelation) {
+      _.forEach(children, function(docRelation){
         docRelation.parentId = currentParent.rowId;
-        docRelation.rowId = docRelation.parentId + '.' + childId++;
+        docRelation.rowId =  docRelation.parentId + '.' + childId++;
 
         $scope.docRelations.push(docRelation);
         $scope.relations.splice(_.findIndex($scope.relations, { 'docId': docRelation.docId }), 1);
 
-        if (_.where($scope.relations, { 'parentDocId': docRelation.docId }).length > 0) {
+        if(_.where($scope.relations, {'parentDocId': docRelation.docId}).length > 0){
           buildHierarchy(docRelation);
         }
       });
@@ -53,16 +43,11 @@
     $scope.viewDoc = function (docId) {
       return $state.go('root.docs.edit.view', { id: docId });
     };
-
-    $scope.gotoAopApp = function () {
-      return $state.go('root.apps.edit', { id: $scope.aopApplicationId });
-    };
   }
 
   DocsCaseCtrl.$inject = [
     '$scope',
     '$state',
-    'Aops',
     'doc'
   ];
 
