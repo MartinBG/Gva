3ac5f58a9be8edfd52fd9d72a7d2f27bad574e91
dimitar.kaludov <mitakyt@gmail.com>
2014-07-21 15:59:12 +0300

    $(Common) Remove validation styles when form is readonly

--- Common.Web.App/js/scaffolding/directives/hasError/hasErrorDirective.js
+++ Common.Web.App/js/scaffolding/directives/hasError/hasErrorDirective.js
@@ -9,12 +9,13 @@
       link: function (scope, element, attrs) {
         scope.form = element.parent().controller('form');
         scope.$watchCollection('[form["' + attrs.scHasError +
-          '"].$invalid, form.$validated, form["' + attrs.scHasError + '"].$error.$pending]',
+          '"].$invalid, form.$validated, form.$readonly, form["' + attrs.scHasError +
+          '"].$error.$pending]',
           function (newValue, oldValue) {
             if (newValue === oldValue) {
               return;
             }
-            if (newValue[0] && newValue[1] && !newValue[2]) {
+            if (newValue[0] && newValue[1] && !newValue[2] && !newValue[3]) {
               element.addClass('has-error');
             }
             else {
