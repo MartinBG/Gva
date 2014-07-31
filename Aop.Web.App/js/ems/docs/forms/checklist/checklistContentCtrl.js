d3a8ab0e7a4419cdf61fdd01078afc353c3e8508
Yanislav Galyov <y.galyov@gmail.com>
2014-07-31 17:38:27 +0300

    $(Aop) improved checklist

--- Aop.Web.App/js/ems/docs/forms/checklist/checklistContentCtrl.js
+++ Aop.Web.App/js/ems/docs/forms/checklist/checklistContentCtrl.js
@@ -9,6 +9,22 @@
     $scope.$watch('model.compareToVersion', function () {
       $('input[type=checkbox]').removeClass('checklist-current-value-checkbox');
       $('input[type=checkbox]').removeClass('checklist-compared-value-checkbox');
+      $('input[type=text]').removeClass('checklist-compared-value-text');
+      $('input[type=text]').removeAttr('title');
+
+      _.each($('input[type=text]'), function (textInput) {
+        if (textInput.attributes['ng-model']) {
+          var ngModelStr = textInput.attributes['ng-model'].value,
+            currentText = $parse(ngModelStr)($scope),
+            compareText =
+              $parse(ngModelStr.replace('currentVersion', 'compareToVersion'))($scope);
+
+          if ($scope.model.isComparing && currentText !== compareText) {
+            textInput.classList.add('checklist-compared-value-text');
+            textInput.setAttribute('title', compareText);
+          }
+        }
+      });
 
       _.each($('input[type=checkbox]'), function (checkbox) {
         var ngModelStr = checkbox.attributes['ng-model'].value,
@@ -16,7 +32,7 @@
           compareCheckbox =
             $parse(ngModelStr.replace('currentVersion', 'compareToVersion'))($scope);
 
-        if($scope.model.isComparing && currentCheckbox !== compareCheckbox) {
+        if ($scope.model.isComparing && currentCheckbox !== compareCheckbox) {
           if (currentCheckbox === true) {
             checkbox.classList.add('checklist-current-value-checkbox');
           } else if (compareCheckbox === true) {
