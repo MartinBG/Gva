22db916ec19cf18e4357e707d9ea482a052d1bfb
Atanas Dzhiljanov <naskodzhil@gmail.com>
2014-08-05 14:15:58 +0300

    $(Common) Add decorators to enable dynamic names in form, ngForm and ngModel directives

---
+++ Common.Web.App/js/scaffolding/decorators/formDecorator.js
@@ -0,0 +1,30 @@
+﻿/*global angular*/
+(function (angular) {
+  'use strict';
+
+  function formDecorator($provide) {
+
+    var decorator = function ($delegate) {
+
+      var form = $delegate[0], controller = form.controller;
+
+      form.controller = ['$scope', '$element', '$attrs', '$injector',
+      function (scope, element, attrs, $injector) {
+        var $interpolate = $injector.get('$interpolate');
+        attrs.$set('name', $interpolate(attrs.name || attrs.ngForm || '')(scope));
+        $injector.invoke(controller, this, {
+          '$scope': scope,
+          '$element': element,
+          '$attrs': attrs
+        });
+      }];
+
+      return $delegate;
+    };
+
+    $provide.decorator('formDirective', ['$delegate', decorator]);
+    $provide.decorator('ngFormDirective', ['$delegate', decorator]);
+  }
+
+  angular.module('scaffolding').config(formDecorator);
+}(angular));
