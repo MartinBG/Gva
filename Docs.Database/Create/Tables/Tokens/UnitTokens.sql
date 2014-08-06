8502fe618d27638803845cb269a62ef94b3344a6
Georgi Yordanov <georgi.yordanov@gmail.com>
2014-08-06 16:40:04 +0300

    $(Docs) Add UnitTokens foreign keys

--- Docs.Database/Create/Tables/Tokens/UnitTokens.sql
+++ Docs.Database/Create/Tables/Tokens/UnitTokens.sql
@@ -3,10 +3,12 @@ GO
 
 CREATE TABLE UnitTokens
 (
-      UnitId int NOT NULL, ----fk
-	  Token nvarchar(200) NOT NULL, 
-	  CreateToken nvarchar(200) NOT NULL, 
-	  ClassificationPermissionId int, ----fk
-	  CONSTRAINT PK_UnitTokens PRIMARY KEY CLUSTERED (UnitId, Token, CreateToken, ClassificationPermissionId)
+    UnitId                      int           NOT NULL,
+    Token                       nvarchar(200) NOT NULL,
+    CreateToken                 nvarchar(200) NOT NULL,
+    ClassificationPermissionId  int           NOT NULL,
+    CONSTRAINT PK_UnitTokens                           PRIMARY KEY (UnitId, Token, CreateToken, ClassificationPermissionId),
+    CONSTRAINT FK_UnitTokens_Units                     FOREIGN KEY (UnitId)                     REFERENCES dbo.Units (UnitId),
+    CONSTRAINT FK_UnitTokens_ClassificationPermissions FOREIGN KEY (ClassificationPermissionId) REFERENCES dbo.ClassificationPermissions (ClassificationPermissionId)
 )
 GO 
