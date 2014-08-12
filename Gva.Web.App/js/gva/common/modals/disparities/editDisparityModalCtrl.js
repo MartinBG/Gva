f21e2d9c366d3e52262e2747bcfa4c16ed894673
PankaLM <pan_lm@abv.bg>
2014-08-12 17:08:55 +0300

    $(Gva) Change disparities in recommendations and inspections to use modal

---
+++ Gva.Web.App/js/gva/common/modals/disparities/editDisparityModalCtrl.js
@@ -0,0 +1,34 @@
+﻿/*global angular*/
+(function (angular) {
+  'use strict';
+
+  function EditDisparityModalCtrl(
+    $scope,
+    $modalInstance,
+    scModalParams
+  ) {
+      $scope.disparity = scModalParams.disparity;
+      $scope.form = {};
+
+      $scope.save = function () {
+        return $scope.form.editDisparityForm.$validate()
+        .then(function () {
+          if ($scope.form.editDisparityForm.$valid) {
+            return $modalInstance.close($scope.disparity);
+          }
+        });
+      };
+
+      $scope.cancel = function () {
+        return $modalInstance.dismiss('cancel');
+      };
+  }
+
+  EditDisparityModalCtrl.$inject = [
+    '$scope',
+    '$modalInstance',
+    'scModalParams'
+  ];
+
+  angular.module('gva').controller('EditDisparityModalCtrl', EditDisparityModalCtrl);
+}(angular));
