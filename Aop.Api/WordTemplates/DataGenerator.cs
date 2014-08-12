f14af05dd4fb87e13bd60dda7cbdd17137c3121d
Yanislav Galyov <y.galyov@gmail.com>
2014-08-12 17:49:38 +0300

    $(Aop) renewal of note and report generation templates

--- Aop.Api/WordTemplates/DataGenerator.cs
+++ Aop.Api/WordTemplates/DataGenerator.cs
@@ -8,8 +8,8 @@ namespace Aop.Api.WordTemplates
     {
         public JObject GenerateNote(
             JObject source,
-            string outgoingNum = "......................................",
-            string date = ".........................................",
+            string outgoingNum = "...",
+            string date = "...",
             string secretaryName = "[СЕКРЕТАР]",
             string secretaryPosition = "/Определен със Заповед № .................../",
             string coordinatorName = "",
@@ -24,9 +24,11 @@ namespace Aop.Api.WordTemplates
                 date = date,
                 paragraph1 = this.GetParagraph1(source.Get<JObject>("paragraph1")),
                 paragraph2 = this.GetParagraph2(source.Get<JObject>("paragraph2")),
+                paragraph3 = this.GetParagraph3(source.Get<JObject>("paragraph3")),
                 paragraph4 = this.GetParagraph4(source.Get<JObject>("paragraph4")),
-                paragraph5 = this.GetParagraph5(source.Get<JObject>("paragraph5")),
                 paragraph6 = this.GetParagraph6(source.Get<JObject>("paragraph6")),
+                paragraph7 = this.GetParagraph7(source.Get<JObject>("paragraph7")),
+                paragraph8 = this.GetParagraph8(source.Get<JObject>("paragraph8")),
                 secretary = new { name = secretaryName, position = secretaryPosition },
                 coordinators = new object[]
                 {
@@ -44,8 +46,8 @@ namespace Aop.Api.WordTemplates
 
         public JObject GenerateReport(
             JObject source,
-            string outgoingNum = "......................................",
-            string date = ".........................................",
+            string outgoingNum = "...",
+            string date = "...",
             string uniqueNum = "",
             string caseManagement = "",
             string directorName = "[ДИРЕКТОР]",
@@ -63,9 +65,11 @@ namespace Aop.Api.WordTemplates
                 caseManagement = caseManagement,
                 paragraph1 = this.GetParagraph1(source.Get<JObject>("paragraph1")),
                 paragraph2 = this.GetParagraph2(source.Get<JObject>("paragraph2")),
+                paragraph3 = this.GetParagraph3(source.Get<JObject>("paragraph3")),
                 paragraph4 = this.GetParagraph4(source.Get<JObject>("paragraph4")),
-                paragraph5 = this.GetParagraph5(source.Get<JObject>("paragraph5")),
                 paragraph6 = this.GetParagraph6(source.Get<JObject>("paragraph6")),
+                paragraph7 = this.GetParagraph7(source.Get<JObject>("paragraph7")),
+                paragraph8 = this.GetParagraph8(source.Get<JObject>("paragraph8")),
                 director = new { name = directorName },
                 coordinators = new object[]
                 {
@@ -113,6 +117,28 @@ namespace Aop.Api.WordTemplates
             };
         }
 
+        private object GetParagraph3(JObject paragraph3)
+        {
+            if (paragraph3 == null)
+            {
+                return null;
+            }
+
+            var decisionText = paragraph3.Get<string>("decision.table1.findings");
+            if (string.IsNullOrEmpty(decisionText))
+            {
+                return null;
+            }
+
+            return new
+            {
+                decision = new
+                {
+                    findings = decisionText
+                }
+            };
+        }
+
         private object GetParagraph4(JObject paragraph4)
         {
             if (paragraph4 == null)
@@ -120,7 +146,7 @@ namespace Aop.Api.WordTemplates
                 return null;
             }
 
-            var techniqueText = paragraph4.Get<string>("technique.table1.row13");
+            var techniqueText = paragraph4.Get<string>("technique.table1.findings");
             if (string.IsNullOrEmpty(techniqueText))
             {
                 return null;
@@ -128,31 +154,9 @@ namespace Aop.Api.WordTemplates
 
             return new
             {
-                text = techniqueText
-            };
-        }
-
-        private object GetParagraph5(JObject paragraph5)
-        {
-            if (paragraph5 == null)
-            {
-                return null;
-            }
-
-            var table1Text = paragraph5.Get<string>("deadline.table1.row12");
-            var table2Text = paragraph5.Get<string>("deadline.table2.row6");
-
-            if (string.IsNullOrEmpty(table1Text) && string.IsNullOrEmpty(table2Text))
-            {
-                return null;
-            }
-
-            return new
-            {
-                deadline = new
+                technique = new
                 {
-                    table1 = string.IsNullOrEmpty(table1Text) ? null : new { text = table1Text },
-                    table2 = string.IsNullOrEmpty(table2Text) ? null : new { text = table2Text }
+                    findings = techniqueText
                 }
             };
         }
@@ -164,11 +168,52 @@ namespace Aop.Api.WordTemplates
                 return null;
             }
 
+            var table1Text = paragraph6.Get<string>("deadline.table1.findings");
+            var table2Text = paragraph6.Get<string>("deadline.table2.findings");
+
+            if (string.IsNullOrEmpty(table1Text) && string.IsNullOrEmpty(table2Text))
+            {
+                return null;
+            }
+
+            return new
+            {
+                deadline = new
+                {
+                    table1 = string.IsNullOrEmpty(table1Text) ? null : new { findings = table1Text },
+                    table2 = string.IsNullOrEmpty(table2Text) ? null : new { findings = table2Text }
+                }
+            };
+        }
+
+        private object GetParagraph7(JObject paragraph7)
+        {
+            if (paragraph7 == null)
+            {
+                return null;
+            }
+
             return new
             {
                 additionalInfo = new
                 {
-                    text = paragraph6.Get<string>("additionalInfo")
+                    text = paragraph7.Get<string>("additionalInfo")
+                }
+            };
+        }
+
+        private object GetParagraph8(JObject paragraph8)
+        {
+            if (paragraph8 == null)
+            {
+                return null;
+            }
+
+            return new
+            {
+                additionalInfo = new
+                {
+                    text = paragraph8.Get<string>("additionalInfo")
                 }
             };
         }
@@ -232,17 +277,18 @@ namespace Aop.Api.WordTemplates
                     tick1 = procedure.Get<bool?>("table1.row7.tick1"),
                     tick2 = procedure.Get<bool?>("table1.row7.tick2"),
                 },
-                financedBy = procedure.Get<string>("table1.row8"),
+                financedBy = new
+                {
+                    tick1 = procedure.Get<bool?>("table1.row8.tick1"),
+                    tick2 = procedure.Get<bool?>("table1.row8.tick2"),
+                    tick3 = procedure.Get<bool?>("table1.row8.tick3")
+                },
                 row1 = new
                 {
                     tick1 = procedure.Get<bool?>("table2.row1.tick1"),
                     tick2 = procedure.Get<bool?>("table2.row1.tick2")
                 },
-                row2 = new
-                {
-                    tick1 = procedure.Get<bool?>("table2.row2.tick1"),
-                    tick2 = procedure.Get<bool?>("table2.row2.tick2")
-                },
+                row2 = procedure.Get<string>("table2.row2"),
                 row3 = new
                 {
                     tick1 = procedure.Get<bool?>("table2.row3.tick1"),
@@ -261,42 +307,49 @@ namespace Aop.Api.WordTemplates
                 row6 = new
                 {
                     tick1 = procedure.Get<bool?>("table2.row6.tick1"),
-                    tick2 = procedure.Get<bool?>("table2.row6.tick2"),
-                    tick3 = procedure.Get<bool?>("table2.row6.tick3"),
-                    tick4 = procedure.Get<bool?>("table2.row6.tick4"),
-                    tick5 = procedure.Get<bool?>("table2.row6.tick5"),
-                    tick6 = procedure.Get<bool?>("table2.row6.tick6"),
-                    tick7 = procedure.Get<bool?>("table2.row6.tick7")
+                    tick2 = procedure.Get<bool?>("table2.row6.tick2")
                 },
-                row7 = procedure.Get<string>("table2.row7")
+                row7 = new
+                {
+                    tick1 = procedure.Get<bool?>("table2.row7.tick1"),
+                    tick2 = procedure.Get<bool?>("table2.row7.tick2")
+                },
+                findings = procedure.Get<string>("table2.findings")
             };
         }
 
         private object GetNotice(JObject notice)
         {
-            var conclusionText = notice.Get<string>("table1.row4");
-            var orderSubjText = notice.Get<string>("table2.row16");
-            var depositsAndGuaranteeText = notice.Get<string>("table3.row9");
-            var clausesText = notice.Get<string>("table4.row3");
-            var economicalOpText = notice.Get<string>("table5.row5");
-            var economicalAndFinancialOppText = notice.Get<string>("table6.row9");
-            var technicalOppText = notice.Get<string>("table7.row8");
-            var procedureText = notice.Get<string>("table8.row10");
-            var administrativeInfoText = notice.Get<string>("table9.row17");
-            var appealProcText = notice.Get<string>("table10.row4");
-            var appendixAText = notice.Get<string>("table11.row3");
-            var appendixBText = notice.Get<string>("table12.row3");
+            string conclusionText = notice.Get<string>("table1.findings"),
+                orderSubjText = notice.Get<string>("table2.findings"),
+                quantityVolumeText = notice.Get<string>("table3.findings"),
+                deadlineText = notice.Get<string>("table4.findings"),
+                depositsAndGuaranteeText = notice.Get<string>("table5.findings"),
+                procurementConditionsText = notice.Get<string>("table6.findings"),
+                economicalOpText = notice.Get<string>("table7.findings"),
+                economicalAndFinancialOppText = notice.Get<string>("table8.findings"),
+                technicalOppText = notice.Get<string>("table9.findings"),
+                othersText = notice.Get<string>("table10.findings"),
+                procedureText = notice.Get<string>("table11.findings"),
+                criteriaAssignmentText = notice.Get<string>("table12.findings"),
+                administrativeInfoText = notice.Get<string>("table13.findings"),
+                appealProcText = notice.Get<string>("table14.findings"),
+                appendixBText = notice.Get<string>("table15.findings");
 
             if (string.IsNullOrEmpty(conclusionText) &&
                 string.IsNullOrEmpty(orderSubjText) &&
+                string.IsNullOrEmpty(quantityVolumeText) &&
+                string.IsNullOrEmpty(deadlineText) &&
                 string.IsNullOrEmpty(depositsAndGuaranteeText) &&
+                string.IsNullOrEmpty(procurementConditionsText) &&
                 string.IsNullOrEmpty(economicalOpText) &&
                 string.IsNullOrEmpty(economicalAndFinancialOppText) &&
                 string.IsNullOrEmpty(technicalOppText) &&
+                string.IsNullOrEmpty(othersText) &&
                 string.IsNullOrEmpty(procedureText) &&
+                string.IsNullOrEmpty(criteriaAssignmentText) &&
                 string.IsNullOrEmpty(administrativeInfoText) &&
                 string.IsNullOrEmpty(appealProcText) &&
-                string.IsNullOrEmpty(appendixAText) &&
                 string.IsNullOrEmpty(appendixBText))
             {
                 return null;
@@ -304,18 +357,21 @@ namespace Aop.Api.WordTemplates
 
             return new
             {
-                conclusion = string.IsNullOrEmpty(conclusionText) ? null : new { text = conclusionText },
-                orderSubj = string.IsNullOrEmpty(orderSubjText) ? null : new { text = orderSubjText },
-                depositsAndGuarantee = string.IsNullOrEmpty(depositsAndGuaranteeText) ? null : new { text = depositsAndGuaranteeText },
-                clauses = string.IsNullOrEmpty(clausesText) ? null : new { text = clausesText },
-                economicalOp = string.IsNullOrEmpty(economicalOpText) ? null : new { text = economicalOpText },
-                economicalAndFinancialOpp = string.IsNullOrEmpty(economicalAndFinancialOppText) ? null : new { text = economicalAndFinancialOppText },
-                technicalOpp = string.IsNullOrEmpty(technicalOppText) ? null : new { text = technicalOppText },
-                procedure = string.IsNullOrEmpty(procedureText) ? null : new { text = procedureText },
-                administrativeInfo = string.IsNullOrEmpty(administrativeInfoText) ? null : new { text = administrativeInfoText },
-                appealProc = string.IsNullOrEmpty(appealProcText) ? null : new { text = appealProcText },
-                appendixA = string.IsNullOrEmpty(appendixAText) ? null : new { text = appendixAText },
-                appendixB = string.IsNullOrEmpty(appendixBText) ? null : new { text = appendixBText }
+                conclusion = string.IsNullOrEmpty(conclusionText) ? null : new { findings = conclusionText },
+                orderSubj = string.IsNullOrEmpty(orderSubjText) ? null : new { findings = orderSubjText },
+                quantityVolume = string.IsNullOrEmpty(quantityVolumeText) ? null : new { findings = quantityVolumeText },
+                deadline = string.IsNullOrEmpty(deadlineText) ? null : new { findings = deadlineText },
+                depositsAndGuarantee = string.IsNullOrEmpty(depositsAndGuaranteeText) ? null : new { findings = depositsAndGuaranteeText },
+                procurementConditions = string.IsNullOrEmpty(procurementConditionsText) ? null : new { findings = procurementConditionsText },
+                economicalOp = string.IsNullOrEmpty(economicalOpText) ? null : new { findings = economicalOpText },
+                economicalAndFinancialOpp = string.IsNullOrEmpty(economicalAndFinancialOppText) ? null : new { findings = economicalAndFinancialOppText },
+                technicalOpp = string.IsNullOrEmpty(technicalOppText) ? null : new { findings = technicalOppText },
+                others = string.IsNullOrEmpty(othersText) ? null : new { findings = othersText },
+                procedure = string.IsNullOrEmpty(procedureText) ? null : new { findings = procedureText },
+                criteriaAssignment = string.IsNullOrEmpty(criteriaAssignmentText) ? null : new { findings = procedureText },
+                administrativeInfo = string.IsNullOrEmpty(administrativeInfoText) ? null : new { findings = administrativeInfoText },
+                appealProc = string.IsNullOrEmpty(appealProcText) ? null : new { findings = appealProcText },
+                appendixB = string.IsNullOrEmpty(appendixBText) ? null : new { findings = appendixBText }
             };
         }
     }
