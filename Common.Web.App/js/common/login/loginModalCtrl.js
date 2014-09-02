1b908be38c1dadd177cdebfae180870c13d96a12
dimitar.kaludov <mitakyt@gmail.com>
2014-09-02 10:37:27 +0300

    $(Gva) Make login form submitable

--- Common.Web.App/js/common/login/loginModalCtrl.js
+++ Common.Web.App/js/common/login/loginModalCtrl.js
@@ -1,20 +1,33 @@
-﻿/*global angular*/
-(function (angular) {
+﻿/*global angular,$*/
+(function (angular, $) {
   'use strict';
 
-  function LoginModalCtrl($scope, $modalInstance, authService) {
+  function LoginModalCtrl($scope, $modalInstance, $timeout, authService) {
     $scope.invalidUsernameAndPassword = false;
-    $scope.login = function (loginForm) {
-      return loginForm
+    $scope.submitting = false;
+
+    $modalInstance.opened.then(function () {
+      $timeout(function () {
+        $('input[name$="username"]').focus();
+        $('input[name$="username"], input[name$="password"]').checkAndTriggerAutoFillEvent();
+      }, 200);
+    });
+
+    $scope.login = function (form, username, password) {
+      return form
         .$validate()
         .then(function () {
-          if (loginForm.$valid) {
+          if (form.$valid && !$scope.submitting) {
+            $scope.submitting = true;
+            $scope.invalidUsernameAndPassword = false;
+
             return authService
-              .authenticate(loginForm.username.$modelValue, loginForm.password.$modelValue)
+              .authenticate(username, password)
               .then(function (success) {
                 if (success) {
                   $modalInstance.close();
                 } else {
+                  $scope.submitting = false;
                   $scope.invalidUsernameAndPassword = true;
                 }
               });
@@ -23,7 +36,7 @@
     };
   }
 
-  LoginModalCtrl.$inject = ['$scope', '$modalInstance', 'authService'];
+  LoginModalCtrl.$inject = ['$scope', '$modalInstance', '$timeout', 'authService'];
 
   angular.module('common').controller('LoginModalCtrl', LoginModalCtrl);
-}(angular));
+}(angular, $));
