dd503fe34ff4acf8b4e1b732f0bc8547eb95335c
dimitar.kaludov <mitakyt@gmail.com>
2014-09-17 13:12:01 +0300

    $(Common) Add query string auth

--- Common.Api/OAuth/ApplicationOAuthBearerProvider.cs
+++ Common.Api/OAuth/ApplicationOAuthBearerProvider.cs
@@ -9,6 +9,8 @@ namespace Common.Api.OAuth
 {
     public class ApplicationOAuthBearerProvider : OAuthBearerAuthenticationProvider
     {
+        private const string AccessToken = "access_token";
+
         public override System.Threading.Tasks.Task ValidateIdentity(OAuthValidateIdentityContext context)
         {
             if (context == null)
@@ -23,5 +25,18 @@ namespace Common.Api.OAuth
 
             return Task.FromResult<object>((object)null);
         }
+
+        public override Task RequestToken(OAuthRequestTokenContext context)
+        {
+            var value = context.Request.Query.Get(AccessToken);
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                context.Token = value;
+            }
+
+            return Task.FromResult<object>(null);
+        }
     }
 }
+ 
