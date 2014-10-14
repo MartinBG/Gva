30dfa1551bacb86ea52f4a8544a64ad83561349c
Albena Kumanova <akumanova@yahoo.com>
2014-10-14 18:15:25 +0300

    $(Gva) Change equipment documentOwners to use GvaCaseTypePartController

--- Gva.Api/Controllers/Equipments/EquipmentOwnersController.cs
+++ Gva.Api/Controllers/Equipments/EquipmentOwnersController.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using System.Web.Http;
+using Common.Api.Models;
 using Common.Api.UserContext;
 using Common.Data;
+using Gva.Api.Models;
 using Gva.Api.ModelsDO;
 using Gva.Api.ModelsDO.Equipments;
 using Gva.Api.Repositories.ApplicationRepository;
+using Gva.Api.Repositories.CaseTypeRepository;
 using Gva.Api.Repositories.FileRepository;
 using Regs.Api.LotEvents;
 using Regs.Api.Repositories.LotRepositories;
@@ -13,44 +16,53 @@ namespace Gva.Api.Controllers.Equipments
 {
     [RoutePrefix("api/equipments/{lotId}/equipmentDocumentOwners")]
     [Authorize]
-    public class EquipmentOwnersController : GvaFilePartController<EquipmentOwnerDO>
+    public class EquipmentOwnersController : GvaCaseTypePartController<EquipmentOwnerDO>
     {
         private IApplicationRepository applicationRepository;
         private ILotRepository lotRepository;
+        private IFileRepository fileRepository;
+        private ICaseTypeRepository caseTypeRepository;
 
         public EquipmentOwnersController(
             IUnitOfWork unitOfWork,
             ILotRepository lotRepository,
             IFileRepository fileRepository,
             IApplicationRepository applicationRepository,
+            ICaseTypeRepository caseTypeRepository,
             ILotEventDispatcher lotEventDispatcher,
             UserContext userContext)
             : base("equipmentDocumentOwners", unitOfWork, lotRepository, fileRepository, lotEventDispatcher, userContext)
         {
             this.applicationRepository = applicationRepository;
             this.lotRepository = lotRepository;
+            this.fileRepository = fileRepository;
+            this.caseTypeRepository = caseTypeRepository;
         }
 
         [Route("new")]
         public IHttpActionResult GetNewOwner(int lotId, int? appId = null)
         {
-            EquipmentOwnerDO newOwner = new EquipmentOwnerDO();
+            GvaCaseType caseType = this.caseTypeRepository.GetCaseTypesForSet("equipment").Single();
+            CaseDO caseDO = new CaseDO()
+            {
+                CaseType = new NomValue()
+                {
+                    NomValueId = caseType.GvaCaseTypeId,
+                    Name = caseType.Name,
+                    Alias = caseType.Alias
+                },
+                BookPageNumber = this.fileRepository.GetNextBPN(lotId, caseType.GvaCaseTypeId).ToString()
+            };
 
-            var cases = new List<CaseDO>();
             if (appId.HasValue)
             {
                 this.lotRepository.GetLotIndex(lotId);
-                cases.Add(new CaseDO()
-                {
-                    IsAdded = true,
-                    Applications = new List<ApplicationNomDO>()
-                    {
-                        this.applicationRepository.GetInitApplication(appId)
-                    }
-                });
+                caseDO.Applications.Add(this.applicationRepository.GetInitApplication(appId));
             }
 
-            return Ok(new FilePartVersionDO<EquipmentOwnerDO>(newOwner, cases));
+            EquipmentOwnerDO newOwner = new EquipmentOwnerDO();
+
+            return Ok(new CaseTypePartDO<EquipmentOwnerDO>(newOwner, caseDO));
         }
     }
 }
