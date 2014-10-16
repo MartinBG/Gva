c3977aa4501df536a617430815b15c6ed95b0ef1
PankaLM <pan_lm@abv.bg>
2014-10-16 17:52:18 +0300

    $(Gva) Change equipment cert operationals to use GvaCAseTypePartController

--- Gva.Api/Controllers/Equipments/EquipmentCertOperationalsController.cs
+++ Gva.Api/Controllers/Equipments/EquipmentCertOperationalsController.cs
@@ -1,10 +1,18 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Common.Api.Models;
 using Common.Api.Repositories.NomRepository;
 using Common.Api.UserContext;
 using Common.Data;
+using Gva.Api.Models;
 using Gva.Api.ModelsDO;
 using Gva.Api.ModelsDO.Equipments;
+using Gva.Api.ModelsDO.Organizations;
 using Gva.Api.Repositories.ApplicationRepository;
+using Gva.Api.Repositories.CaseTypeRepository;
+using Gva.Api.Repositories.FileRepository;
+using Gva.Api.Repositories.OrganizationRepository;
 using Regs.Api.LotEvents;
 using Regs.Api.Repositories.LotRepositories;
 
@@ -12,9 +20,11 @@ namespace Gva.Api.Controllers.Equipments
 {
     [RoutePrefix("api/equipments/{lotId}/equipmentCertOperationals")]
     [Authorize]
-    public class EquipmentCertOperationalsController : GvaApplicationPartController<EquipmentCertOperationalDO>
+    public class EquipmentCertOperationalsController : GvaCaseTypePartController<EquipmentCertOperationalDO>
     {
         private INomRepository nomRepository;
+        private ICaseTypeRepository caseTypeRepository;
+        private IFileRepository fileRepository;
 
         public EquipmentCertOperationalsController(
             IUnitOfWork unitOfWork,
@@ -22,19 +32,37 @@ namespace Gva.Api.Controllers.Equipments
             IApplicationRepository applicationRepository,
             INomRepository nomRepository,
             ILotEventDispatcher lotEventDispatcher,
+            ICaseTypeRepository caseTypeRepository,
+            IFileRepository fileRepository,
             UserContext userContext)
-            : base("equipmentCertOperationals", unitOfWork, lotRepository, applicationRepository, lotEventDispatcher, userContext)
+            : base("equipmentCertOperationals", unitOfWork, lotRepository, fileRepository, lotEventDispatcher, userContext)
         {
             this.nomRepository = nomRepository;
+            this.fileRepository = fileRepository;
+            this.caseTypeRepository = caseTypeRepository;
         }
 
         [Route("new")]
         public IHttpActionResult GetNewCertOperational(int lotId)
         {
-            EquipmentCertOperationalDO newCertOperational = new EquipmentCertOperationalDO();
-            newCertOperational.Valid = this.nomRepository.GetNomValue("boolean", "yes");
+           var caseType = this.caseTypeRepository.GetCaseTypesForSet("Aircraft").Single();
+           var caseDO = new CaseDO()
+           {
+               CaseType = new NomValue()
+               {
+                   NomValueId = caseType.GvaCaseTypeId,
+                   Name = caseType.Name,
+                   Alias = caseType.Alias
+               },
+               IsAdded = true
+           };
 
-            return Ok(new ApplicationPartVersionDO<EquipmentCertOperationalDO>(newCertOperational));
+           EquipmentCertOperationalDO newCertOperational = new EquipmentCertOperationalDO()
+           {
+               Valid = this.nomRepository.GetNomValue("boolean", "yes")
+           };
+
+           return Ok(new CaseTypePartDO<EquipmentCertOperationalDO>(newCertOperational, caseDO));
         }
     }
 }
