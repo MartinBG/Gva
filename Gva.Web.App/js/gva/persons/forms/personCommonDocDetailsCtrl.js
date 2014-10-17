3b0c21835e19040f9d019fb9baf58a6a7b887989
PankaLM <pan_lm@abv.bg>
2014-10-17 14:25:59 +0300

    $(Gva) Create new common doc form for person checks, lang certs and trainings.

---
+++ Gva.Web.App/js/gva/persons/forms/personCommonDocDetailsCtrl.js
@@ -0,0 +1,13 @@
+﻿/*global angular*/
+(function (angular) {
+  'use strict';
+
+  function PersonCommonDocDetailsCtrl($scope, scFormParams) {
+    $scope.caseTypeId = scFormParams.caseTypeId;
+    $scope.categoryAlias = scFormParams.categoryAlias;
+  }
+
+  PersonCommonDocDetailsCtrl.$inject = ['$scope', 'scFormParams'];
+
+  angular.module('gva').controller('PersonCommonDocDetailsCtrl', PersonCommonDocDetailsCtrl);
+}(angular));
