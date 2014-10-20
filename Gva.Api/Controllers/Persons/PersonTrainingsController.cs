42dd13c09b64bdf892c0a6de215b6887358eb12f
Albena Kumanova <akumanova@yahoo.com>
2014-10-20 15:30:47 +0300

    $(Gva) Change PersonTrainings init method

--- Gva.Api/Controllers/Persons/PersonTrainingsController.cs
+++ Gva.Api/Controllers/Persons/PersonTrainingsController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Web.Http;
 using Common.Api.Repositories.NomRepository;
 using Common.Api.UserContext;
 using Common.Data;
 using Gva.Api.ModelsDO;
 using Gva.Api.ModelsDO.Persons;
-using Gva.Api.Repositories.ApplicationRepository;
 using Gva.Api.Repositories.FileRepository;
 using Regs.Api.LotEvents;
 using Regs.Api.Repositories.LotRepositories;
@@ -17,50 +15,30 @@ namespace Gva.Api.Controllers.Persons
     [Authorize]
     public class PersonTrainingsController : GvaCaseTypePartController<PersonTrainingDO>
     {
-        private IApplicationRepository applicationRepository;
-        private ILotRepository lotRepository;
         private INomRepository nomRepository;
 
         public PersonTrainingsController(
             IUnitOfWork unitOfWork,
             ILotRepository lotRepository,
             IFileRepository fileRepository,
-            IApplicationRepository applicationRepository,
             INomRepository nomRepository,
             ILotEventDispatcher lotEventDispatcher,
             UserContext userContext)
             : base("personDocumentTrainings", unitOfWork, lotRepository, fileRepository, lotEventDispatcher, userContext)
         {
-            this.applicationRepository = applicationRepository;
-            this.lotRepository = lotRepository;
             this.nomRepository = nomRepository;
         }
 
         [Route("new")]
-        public IHttpActionResult GetNewTraining(int lotId, int? appId = null)
+        public IHttpActionResult GetNewTraining(int lotId)
         {
             PersonTrainingDO newTraining = new PersonTrainingDO()
             {
-                DocumentDateValidFrom = DateTime.Now
+                DocumentDateValidFrom = DateTime.Now,
+                Valid = this.nomRepository.GetNomValue("boolean", "yes")
             };
 
-            newTraining.Valid = this.nomRepository.GetNomValue("boolean", "yes");
-
-            CaseDO caseDO = null;
-            if (appId.HasValue)
-            {
-                this.lotRepository.GetLotIndex(lotId);
-                caseDO = new CaseDO()
-                {
-                    IsAdded = true,
-                    Applications = new List<ApplicationNomDO>()
-                    {
-                        this.applicationRepository.GetInitApplication(appId)
-                    }
-                };
-            }
-
-            return Ok(new CaseTypePartDO<PersonTrainingDO>(newTraining, caseDO));
+            return Ok(new CaseTypePartDO<PersonTrainingDO>(newTraining));
         }
     }
 }
