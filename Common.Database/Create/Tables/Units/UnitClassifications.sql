96192acf27647dabd09115a81ff9bb270a3dd770
PankaLM <pan_lm@abv.bg>
2014-11-24 09:59:13 +0200

    $(Common, Aop) Fix sql files identation and add small style changes

--- Common.Database/Create/Tables/Units/UnitClassifications.sql
+++ Common.Database/Create/Tables/Units/UnitClassifications.sql
@@ -3,15 +3,15 @@ GO
 
 CREATE TABLE UnitClassifications
 (
-    UnitClassificationId			INT				IDENTITY (1, 1) NOT NULL,
-	UnitId					INT				NOT NULL,
-	ClassificationId		INT				NOT NULL,
-	ClassificationPermissionId		INT				NOT NULL,
-    Version					ROWVERSION     NOT NULL,
+    UnitClassificationId        INT IDENTITY (1, 1) NOT NULL,
+    UnitId                      INT                 NOT NULL,
+    ClassificationId            INT                 NOT NULL,
+    ClassificationPermissionId  INT                 NOT NULL,
+    Version                     ROWVERSION          NOT NULL,
     CONSTRAINT PK_UnitClassifications PRIMARY KEY CLUSTERED (UnitClassificationId),
-	CONSTRAINT [FK_UnitClassifications_Units] FOREIGN KEY ([UnitId]) REFERENCES [dbo].[Units] ([UnitId]),
-	CONSTRAINT [FK_UnitClassifications_Classifications] FOREIGN KEY ([ClassificationId]) REFERENCES [dbo].[Classifications] ([ClassificationId]),
-	CONSTRAINT [FK_UnitClassifications_ClassificationPermissions] FOREIGN KEY ([ClassificationPermissionId]) REFERENCES [dbo].[ClassificationPermissions] ([ClassificationPermissionId]),
+    CONSTRAINT [FK_UnitClassifications_Units] FOREIGN KEY ([UnitId]) REFERENCES [dbo].[Units] ([UnitId]),
+    CONSTRAINT [FK_UnitClassifications_Classifications] FOREIGN KEY ([ClassificationId]) REFERENCES [dbo].[Classifications] ([ClassificationId]),
+    CONSTRAINT [FK_UnitClassifications_ClassificationPermissions] FOREIGN KEY ([ClassificationPermissionId]) REFERENCES [dbo].[ClassificationPermissions] ([ClassificationPermissionId]),
 )
 GO 
 
