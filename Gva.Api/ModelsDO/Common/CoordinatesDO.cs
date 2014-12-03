99bf0d6621adab283ddb0fefe331c1fdd048aa0a
PankaLM <pan_lm@abv.bg>
2014-12-03 15:21:57 +0200

    $(Gva) Fix CoordinatesDO

--- Gva.Api/ModelsDO/Common/CoordinatesDO.cs
+++ Gva.Api/ModelsDO/Common/CoordinatesDO.cs
@@ -4,10 +4,10 @@ namespace Gva.Api.ModelsDO.Common
 {
     public class CoordinatesDO
     {
-        [Range(-180, 180, ErrorMessage = "Longitude isn't between 1 and 100.")]
+        [Range(-180, 180, ErrorMessage = "Longitude isn't between -180 and 180.")]
         public double? Longitude { get; set; }
 
-        [Range(-180, 180, ErrorMessage = "Latitude isn't between 1 and 100.")]
+        [Range(-90, 90, ErrorMessage = "Latitude isn't between -90 and 90.")]
         public double? Latitude { get; set; }
     }
 }
