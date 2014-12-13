ee6150257cb281eefaf35033d757aae7fa4f3e9c
PankaLM <pan_lm@abv.bg>
2014-12-13 16:57:52 +0200

    $(Gva) Add classification for staff examiner case type.

--- Gva.Database/Insert/GvaExcelConfig/Classifications.sql
+++ Gva.Database/Insert/GvaExcelConfig/Classifications.sql
@@ -12,18 +12,19 @@ INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUE
 INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(8,N'ТО(AML)',N'to_vs',1);
 INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(9,N'ТО(СУВД)',N'to_suvd',1);
 INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(10,N'ПКС',N'security',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(11,N'ВС',N'aircraft',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(12,N'ЛЛП',N'airport',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(13,N'ССНО',N'equipment',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(14,N'Организации',N'',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(15,N'Общи',N'org',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(16,N'ОО',N'approvedOrg',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(17,N'ЛО',N'airportOperator',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(18,N'АУЦ',N'educationOrg',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(19,N'ВП',N'airCarrier',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(20,N'САО',N'airOperator',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(21,N'СИАНО',N'airNavSvcProvider',1);
-INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(22,N'НСВНК и АСУВД',N'groundSvcOperator',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(11,N'Проверяващ лица',N'staffExaminer',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(12,N'ВС',N'aircraft',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(13,N'ЛЛП',N'airport',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(14,N'ССНО',N'equipment',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(15,N'Организации',N'',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(16,N'Общи',N'org',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(17,N'ОО',N'approvedOrg',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(18,N'ЛО',N'airportOperator',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(19,N'АУЦ',N'educationOrg',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(20,N'ВП',N'airCarrier',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(21,N'САО',N'airOperator',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(22,N'СИАНО',N'airNavSvcProvider',1);
+INSERT INTO [Classifications]([ClassificationId],[Name],[Alias],[IsActive])VALUES(23,N'НСВНК и АСУВД',N'groundSvcOperator',1);
 SET IDENTITY_INSERT [Classifications] OFF
 GO
 
