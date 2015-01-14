f76eda4a99a67b75b54b01186a08b77e5d588b57
Georgi Yordanov <georgi.yordanov@gmail.com>
2015-01-14 17:27:40 +0200

    Fix nom scripts

--- Common.Database/Insert/Addresses/districts.sql
+++ Common.Database/Insert/Addresses/districts.sql
@@ -1,1 +1,64 @@
-﻿PRINT 'Insert Districts'GOSET IDENTITY_INSERT [dbo].[Districts] ON GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (1, N'BLG', N'01', N'BG41', N'Благоевград', N'04279', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (2, N'BGS', N'02', N'BG34', N'Бургас', N'07079', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (3, N'VAR', N'03', N'BG33', N'Варна', N'10135', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (4, N'VTR', N'04', N'BG32', N'Велико Търново', N'10447', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (5, N'VID', N'05', N'BG31', N'Видин', N'10971', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (6, N'VRC', N'06', N'BG31', N'Враца', N'12259', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (7, N'GAB', N'07', N'BG32', N'Габрово', N'14218', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (8, N'DOB', N'08', N'BG33', N'Добрич', N'72624', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (9, N'KRZ', N'09', N'BG42', N'Кърджали', N'40909', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (10, N'KNL', N'10', N'BG41', N'Кюстендил', N'41112', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (11, N'LOV', N'11', N'BG31', N'Ловеч', N'43952', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (12, N'MON', N'12', N'BG31', N'Монтана', N'48489', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (13, N'PAZ', N'13', N'BG42', N'Пазарджик', N'55155', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (14, N'PER', N'14', N'BG41', N'Перник', N'55871', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (15, N'PVN', N'15', N'BG31', N'Плевен', N'56722', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (16, N'PDV', N'16', N'BG42', N'Пловдив', N'56784', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (17, N'RAZ', N'17', N'BG32', N'Разград', N'61710', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (18, N'RSE', N'18', N'BG32', N'Русе', N'63427', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (19, N'SLS', N'19', N'BG32', N'Силистра', N'66425', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (20, N'SLV', N'20', N'BG34', N'Сливен', N'67338', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (21, N'SML', N'21', N'BG42', N'Смолян', N'67653', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (22, N'SOF', N'22', N'BG41', N'София (столица)', N'68134', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (23, N'SFO', N'23', N'BG41', N'София', N'68134', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (24, N'SZR', N'24', N'BG34', N'Стара Загора', N'68850', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (25, N'TGV', N'25', N'BG33', N'Търговище', N'73626', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (26, N'HKV', N'26', N'BG42', N'Хасково', N'77195', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (27, N'SHU', N'27', N'BG33', N'Шумен', N'83510', NULL, NULL, 1)GOINSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (28, N'JAM', N'28', N'BG34', N'Ямбол', N'87374', NULL, NULL, 1)GOSET IDENTITY_INSERT [dbo].[Districts] OFFGO
+﻿PRINT 'Insert Districts'
+GO
+
+SET IDENTITY_INSERT [dbo].[Districts] ON 
+
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (1, N'BLG', N'01', N'BG41', N'Благоевград', N'04279', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (2, N'BGS', N'02', N'BG34', N'Бургас', N'07079', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (3, N'VAR', N'03', N'BG33', N'Варна', N'10135', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (4, N'VTR', N'04', N'BG32', N'Велико Търново', N'10447', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (5, N'VID', N'05', N'BG31', N'Видин', N'10971', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (6, N'VRC', N'06', N'BG31', N'Враца', N'12259', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (7, N'GAB', N'07', N'BG32', N'Габрово', N'14218', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (8, N'DOB', N'08', N'BG33', N'Добрич', N'72624', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (9, N'KRZ', N'09', N'BG42', N'Кърджали', N'40909', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (10, N'KNL', N'10', N'BG41', N'Кюстендил', N'41112', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (11, N'LOV', N'11', N'BG31', N'Ловеч', N'43952', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (12, N'MON', N'12', N'BG31', N'Монтана', N'48489', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (13, N'PAZ', N'13', N'BG42', N'Пазарджик', N'55155', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (14, N'PER', N'14', N'BG41', N'Перник', N'55871', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (15, N'PVN', N'15', N'BG31', N'Плевен', N'56722', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (16, N'PDV', N'16', N'BG42', N'Пловдив', N'56784', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (17, N'RAZ', N'17', N'BG32', N'Разград', N'61710', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (18, N'RSE', N'18', N'BG32', N'Русе', N'63427', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (19, N'SLS', N'19', N'BG32', N'Силистра', N'66425', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (20, N'SLV', N'20', N'BG34', N'Сливен', N'67338', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (21, N'SML', N'21', N'BG42', N'Смолян', N'67653', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (22, N'SOF', N'22', N'BG41', N'София (столица)', N'68134', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (23, N'SFO', N'23', N'BG41', N'София', N'68134', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (24, N'SZR', N'24', N'BG34', N'Стара Загора', N'68850', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (25, N'TGV', N'25', N'BG33', N'Търговище', N'73626', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (26, N'HKV', N'26', N'BG42', N'Хасково', N'77195', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (27, N'SHU', N'27', N'BG33', N'Шумен', N'83510', NULL, NULL, 1)
+GO
+INSERT [dbo].[Districts] ([DistrictId], [Code], [Code2], [SecondLevelRegionCode], [Name], [MainSettlementCode], [Alias], [Description], [IsActive]) VALUES (28, N'JAM', N'28', N'BG34', N'Ямбол', N'87374', NULL, NULL, 1)
+GO
+SET IDENTITY_INSERT [dbo].[Districts] OFF
+GO
