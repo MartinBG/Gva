3093e24cf9b5f3c0bf12d7a3d58fadfedd2e2828
PankaLM <pan_lm@abv.bg>
2015-02-27 15:31:14 +0200

    Implement creation of owner from certRegFm form

--- Gva.Web.App/js/gva/aircrafts/forms/aircraftCertRegFMCtrl.js
+++ Gva.Web.App/js/gva/aircrafts/forms/aircraftCertRegFMCtrl.js
@@ -4,13 +4,52 @@
 
   function AircraftCertRegFMCtrl(
     $scope,
+    Nomenclatures,
+    scModal,
     gvaConstants
     ) {
     $scope.regMarkPattern = gvaConstants.regMarkPattern;
+
+    $scope.newOwner = function () {
+      if ($scope.model.ownerIsOrg) {
+        var modalNewOrganization = scModal.open('newOrganization');
+
+        modalNewOrganization.result.then(function (organizationId) {
+          Nomenclatures.get({
+            alias: 'organizations',
+            id: organizationId
+          })
+          .$promise
+          .then(function (organization) {
+            $scope.model.ownerOrganization = organization;
+          });
+        });
+
+        return modalNewOrganization.opened;
+      } else {
+        var modalNewOPerson = scModal.open('newPerson');
+
+        modalNewOPerson.result.then(function (personId) {
+          Nomenclatures.get({
+            alias: 'persons',
+            id: personId
+          })
+          .$promise
+          .then(function (person) {
+            $scope.model.ownerPerson = person;
+          });
+        });
+
+        return modalNewOPerson.opened;
+      }
+    };
+   
   }
 
   AircraftCertRegFMCtrl.$inject = [
     '$scope',
+    'Nomenclatures',
+    'scModal',
     'gvaConstants'
   ];
 
