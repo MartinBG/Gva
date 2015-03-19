ad951bedd9f1b19aa12c08ae3a0839b276241b7e
Kalin Molerov <kalin.molerov@abbaty.com>
2015-03-19 17:23:58 +0200

    Added new projects and solution for Docs system - Web App and Hosting App
    Fix and config to Docs.Web.App project.
    
    Ems solution changed to Docs, because it is confusing. Solution is used only for dev of Docs backend.

---
+++ Docs.Web.Host/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Common;
+using Common.Api;
+using Common.Owin;
+using Docs.Api;
+using Owin;
+using Rio.Data;
+
+namespace Ems.Web.Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var container = CreateAutofacContainer();
+
+            App.Configure(app, container);
+        }
+
+        public static IContainer CreateAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new CommonModule());
+            builder.RegisterModule(new CommonApiModule());
+            builder.RegisterModule(new DocsApiModule());
+            builder.RegisterModule(new RioDataModule());
+
+            return builder.Build();
+        }
+    }
+}
