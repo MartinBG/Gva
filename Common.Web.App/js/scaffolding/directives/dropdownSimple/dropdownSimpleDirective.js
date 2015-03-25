d4b6d71f9c8cd2448571a88290cb54de7393d32c
Kalin Molerov <kalin.molerov@abbaty.com>
2015-03-25 17:00:03 +0200

    New Scafolding component for simple Drop down, supporting data binding
    changes to DD directive

---
+++ Common.Web.App/js/scaffolding/directives/dropdownSimple/dropdownSimpleDirective.js
@@ -0,0 +1,42 @@
+﻿/* Usage:
+  <sc-dropdown-simple 
+    items-source="dropdownModel"
+    selected-item="selectedItem" 
+    selected-value="selectedValue"
+    item-text-field="name"
+    item-value-field="value">
+  </sc-dropdown-simple>>
+*/
+
+// From outside select it only trough Selected Item, not trough Value
+
+/*global angular*/
+(function (angular) {
+  'use strict';
+
+  function DropdownSimpleDirective() {
+
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'js/scaffolding/directives/dropdownSimple/dropdownSimpleDirective.html',
+      scope: {
+        selectedItem: "=",
+        selectedValue: "=",
+        itemsSource: '&',
+        itemTextField: '@',
+        itemValueField: '@'
+      },
+      link: function (scope, element, attrs) {
+        scope.selectItem = function (item) {
+          scope.selectedItem = item;
+          scope.selectedValue = item[scope.itemValueField];
+        };
+      }
+    };
+  }
+
+  DropdownSimpleDirective.$inject = [];
+
+  angular.module('scaffolding').directive('scDropdownSimple', DropdownSimpleDirective);
+}(angular));
