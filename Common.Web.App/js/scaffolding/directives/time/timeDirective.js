d9f20090966fe3d981aebb21ef07796a7ed9d4c7
PankaLM <pan_lm@abv.bg>
2015-04-14 16:16:47 +0300

    Fix som bugs in time directive

--- Common.Web.App/js/scaffolding/directives/time/timeDirective.js
+++ Common.Web.App/js/scaffolding/directives/time/timeDirective.js
@@ -9,7 +9,7 @@
       priority: 110,
       restrict: 'E',
       replace: true,
-      require: '?ngModel',
+      require: 'ngModel',
       scope: {
       },
       templateUrl: 'js/scaffolding/directives/time/timeDirective.html',
@@ -24,6 +24,15 @@
           scope.isReadonly = !!value;
         });
 
+        ngModel.$render = function () {
+          if (ngModel.$viewValue) {
+            var minutes = ngModel.$viewValue / 60000;
+
+            scope.hours = Math.floor(minutes / 60);
+            scope.minutes = minutes % 60;
+          }
+        };
+
         function calculateMilliseconds() {
           var hours = scope.hours ? scope.hours : 0,
             minutes = scope.minutes ? scope.minutes : 0,
@@ -35,27 +44,28 @@
           }
         }
 
-        ngModel.$render = function () {
-          if (ngModel.$viewValue) {
-            var minutes = ngModel.$viewValue / 60000;
-
-            scope.hours = Math.floor(minutes / 60);
-            scope.minutes = minutes % 60;
-          }
-        };
-
+        var initializingMinutes = true;
         scope.$watch('minutes', function (value) {
-          scope.minutes = value > 59 ? undefined : value;
+          if (initializingMinutes) {
+            initializingMinutes = false;
+          } else {
+            scope.minutes = value > 59 ? undefined : value;
 
-          calculateMilliseconds();
+            calculateMilliseconds();
+          }
         });
 
+        var initializingHours = true;
         scope.$watch('hours', function (value) {
-          if (!infinityMode) {
-            scope.hours = value > 23 ? undefined : value;
-          }
+          if (initializingHours) {
+            initializingHours = false;
+          } else {
+            if (!infinityMode) {
+              scope.hours = value;
+            }
 
-          calculateMilliseconds();
+            calculateMilliseconds();
+          }
         });
       }
     };
