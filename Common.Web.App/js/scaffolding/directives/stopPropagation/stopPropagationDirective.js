ef7cae5a6f07055d2526b27f9ff2a17e2779705b
Kalin Molerov <kalin.molerov@abbaty.com>
2015-04-14 13:19:01 +0300

    Directive for stoping event propagation. Used as attribute.

---
+++ Common.Web.App/js/scaffolding/directives/stopPropagation/stopPropagationDirective.js
@@ -0,0 +1,30 @@
+﻿// Usage: <button ng-click="someHandler() sc-stop-propagation="click"> </button>
+// Usage: <button ng-click="someHandler() sc-stop-propagation="dblclick"> </button>
+// Usage: <button ng-click="someHandler() sc-stop-propagation> </button> default will stop click event
+
+/*global angular, $*/
+(function (angular, $) {
+  'use strict';
+
+  function StopPropagationDirective() {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attr) {
+        if (attr.scStopPropagation) {
+          $(element).bind(attr.scStopPropagation, function (e) {
+            e.stopPropagation();
+          });
+        }
+        else {
+          $(element).click(function (e) {
+            e.stopPropagation();
+          });
+        }
+      }
+    };
+  }
+
+  StopPropagationDirective.$inject = [];
+
+  angular.module('scaffolding').directive('scStopPropagation', StopPropagationDirective);
+}(angular, $));
