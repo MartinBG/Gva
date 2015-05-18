96099eb46691ae53dd2e059574ec68e8c399c6a9
PankaLM <pan_lm@abv.bg>
2015-05-18 16:11:50 +0300

    Fix mistaken label of aircarft main view

--- Gva.Web.App/js/gva/gva_bg-bg.js
+++ Gva.Web.App/js/gva/gva_bg-bg.js
@@ -3932,7 +3932,7 @@
         'root.persons.view.reports.new': 'Нов отчет',
         'root.persons.view.reports.edit': 'Преглед на отчет',
         'root.persons.view.examinationSystem': 'Изпитна система',
-        'root.aircrafts': 'Списък ВС',
+        'root.aircrafts': 'ВС',
         'root.aircrafts.registrations': 'Регистрации на Въздухоплавателни средства',
         'root.aircrafts.invalidActNumbers': 'Невалидни деловодни номера',
         'root.aircrafts.new': 'Ново ВС',
